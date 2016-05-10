a09fd3d41f0ca7217b6b52bb28d16b7dae24c106
Rhys Evans <wheresrhys@gmail.com>
2016-05-10 10:19:05 +0100

    More changes to work on stream page (#6)
    *  adapted to work with stream-page
    
    * remove expander initialisation
    
    * initialise is silent variable

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 import oDate from './date';
-
 import layout from 'n-layout';
 import setup from 'next-js-setup';
 import prompts from './message-prompts';
 import { client as myFtClient, ui as myFtUi } from './myft';
 
 module.exports = {
-	bootstrap: function (cb) {
-		return setup.bootstrap(({ flags }, opts = {}) => {
+	bootstrap: function (cb, opts = {}) {
+		return setup.bootstrap(({ flags }) => {
 			// NOTE: make sure we init myft client *before* n-layout
 			const clientOpts = [];
 			flags.get('follow') && clientOpts.push({relationship: 'followed', type: 'concept'});
