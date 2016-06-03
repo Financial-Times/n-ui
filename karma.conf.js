194caeef30da4fd5945079f2ca7b7fbfa99917a9
Rhys Evans <rhys.evans@ft.com>
2016-06-03 10:51:35 +0100

    added babel-runtime

--- karma.conf.js
+++ karma.conf.js
@@ -24,18 +24,18 @@ module.exports = function (config) {
 
 		// list of files / patterns to load in the browser
 		files: [
-			'//cdn.polyfill.io/v2/polyfill.min.js?callback=ftNextPolyfillServiceCallback&features=' + [
-				'default',
-				'requestAnimationFrame',
-				'Promise',
-				'matchMedia',
-				'HTMLPictureElement',
-				// the following polyfills are included pending https://github.com/Financial-Times/polyfill-service/issues/653
-				'CustomEvent|always|gated',
-				'fetch|always|gated',
-				'Array.prototype.find|always|gated',
-				'Array.prototype.findIndex|always|gated'
-			].join(',') + '&excludes=Symbol,Symbol.iterator,Symbol.species,Map,Set'
+			// '//cdn.polyfill.io/v2/polyfill.min.js?callback=ftNextPolyfillServiceCallback&features=' + [
+			// 	'default',
+			// 	'requestAnimationFrame',
+			// 	'Promise',
+			// 	'matchMedia',
+			// 	'HTMLPictureElement',
+			// 	// the following polyfills are included pending https://github.com/Financial-Times/polyfill-service/issues/653
+			// 	'CustomEvent|always|gated',
+			// 	'fetch|always|gated',
+			// 	'Array.prototype.find|always|gated',
+			// 	'Array.prototype.findIndex|always|gated'
+			// ].join(',') + '&excludes=Symbol,Symbol.iterator,Symbol.species,Map,Set'
 		].concat(componentsToTest.map(name => name + '/test/*.spec.js')),
 
 		// preprocess matching files before serving them to  the browser
