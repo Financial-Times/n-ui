fb83f911fe6eb47792f85bffd8b801494fb521e3
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-06-06 14:45:28 +0100

    Move new header JS flags into n-ui

--- header/index.js
+++ header/index.js
@@ -1,1 +1,19 @@
-module.exports = require('n-header-footer');
+function init (flags) {
+
+	if (flags.get('newHeader')) {
+		require('n-header-footer/drawer').init();
+	} else {
+		require('n-header-footer').init();
+	}
+
+	if (flags.get('meganav')) {
+		require('n-header-footer/meganav').init();
+	}
+
+	if (flags.get('sectionBreadcrumbs')) {
+		require('n-header-footer/breadcrumb').init();
+	}
+
+}
+
+module.exports = { init };
