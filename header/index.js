0a6183e78af0aee00a91addf41da52e033c243fb
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-06-09 16:40:34 +0100

    Initialise new header when enabled

--- header/index.js
+++ header/index.js
@@ -1,14 +1,18 @@
 function init (flags) {
 
 	if (flags.get('newHeader')) {
+		require('n-header-footer/header_new').init();
+
+		// TODO: flag this
 		require('n-header-footer/drawer').init();
+
+		if (flags.get('meganav')) {
+			require('n-header-footer/meganav').init();
+		}
 	} else {
 		require('n-header-footer').init(flags);
 	}
 
-	if (flags.get('meganav')) {
-		require('n-header-footer/meganav').init();
-	}
 
 	if (flags.get('sectionBreadcrumbs')) {
 		require('n-header-footer/breadcrumb').init();
