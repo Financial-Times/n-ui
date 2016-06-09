13353a6629a69da2d71b5ec73c5e7248ac0cdc6b
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-06-09 17:40:12 +0100

    Drawer is now flagged

--- header/index.js
+++ header/index.js
@@ -3,8 +3,9 @@ function init (flags) {
 	if (flags.get('newHeader')) {
 		require('n-header-footer/header_new').init();
 
-		// TODO: flag this
-		require('n-header-footer/drawer').init();
+		if (flags.get('fancyDrawer')) {
+			require('n-header-footer/drawer').init();
+		}
 
 		if (flags.get('meganav')) {
 			require('n-header-footer/meganav').init();
