e3c38339e69b8cd0495024ef94a3a5c2e83e4ae4
Rhys Evans <rhys.evans@ft.com>
2016-06-13 10:09:10 +0100

    cache-control

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -1,5 +1,5 @@
 const shellpromise = require('shellpromise');
 const version = require("../bower_components/n-ui/.bower.json").version.split(".").shift();
 
-shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/v${version}/ --strip 1 --bucket ft-next-n-ui-prod`, { verbose: true, env: process.env })
+shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/v${version}/ --strip 1 --bucket ft-next-n-ui-prod --cache-control 'no-cache, must-revalidate, max-age=3600'`, { verbose: true, env: process.env })
 	.catch(console.log.bind(console))
