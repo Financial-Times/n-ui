842d1c5de775e1ed5957ec064e208a29475e4636
Rhys Evans <wheresrhys@gmail.com>
2016-06-15 15:26:30 +0100

    deploy on tag and handle betas (#118)

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -1,5 +1,9 @@
 const shellpromise = require('shellpromise');
-const version = require("../bower_components/n-ui/.bower.json").version.split(".").slice(0,2).join('.');
+let version = require("../bower_components/n-ui/.bower.json").version;
+
+if (!/(beta|rc)/.test(version)) {
+	version = version.split(".").slice(0,2).join('.');
+}
 
 //TODO soft purge fastly too
 shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/v${version}/ --strip 1 --bucket ft-next-n-ui-prod --cache-control 'no-cache, must-revalidate, max-age=3600'`, { verbose: true, env: process.env })
