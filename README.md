23621b0e72979724aebbffc2bf37ec448ea06565
Paul Wilson <paul.i.wilson@ft.com>
2016-06-17 21:00:31 +0100

    making work behind a flag

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ An experiment bunching some of Next's client-side code into a single repository.
 
 ### Bootstrapping your app
 
-Sass and JS follow similar apis. Both expect a map of feature names (see main.js or configure.scss for feature lists), and also recognise 2 presets:
+Sass and JS follow similar apis. Both expect a map of feature names (see index.js or configure.scss for feature lists), and also recognise 2 presets:
 
 - `complete`: all the bits and pieces you need to style and add interactivity to a typical page on next, complete with MyFT, alerts, promos, ads, tracking etc.
 - `discrete`: provides styles and scripts for just the header, footer and essential pieces of functionality such as tracking. For use on pages where we want no distractions E.G. errors, login and other forms.
---
+++ welcome-message/new/index.js
@@ -0,0 +1,108 @@
+const superstore = require('superstore-sync');
+
+const IOS_DEVICE_REGEX = /OS [0-9]{1,2}(_[0-9]){1,2} like Mac OS X/i;
+const ANDROID_DEVICE_REGEX = /Android/i;
+
+function isWebAppCapableDevice(userAgent){
+	return IOS_DEVICE_REGEX.test(userAgent);
+}
+
+function isAndroidDevice(userAgent){
+	return ANDROID_DEVICE_REGEX.test(userAgent);
+}
+
+function addClass (element, className) {
+	element.classList.add(className);
+}
+
+function removeClass (element, className) {
+	element.classList.remove(className);
+}
+
+function store (key, value) {
+	superstore.local.set(key, value);
+}
+
+function retrieve (key) {
+	return superstore.local.get(key);
+}
+
+function onButtonClose (func, storageKey, event) {
+	event.preventDefault();
+	store(storageKey, 1);
+	func();
+}
+
+function fixBarSetup (floatingElement, footerElement, visibleClass, hiddenClass) {
+	return () => {
+		addClass(floatingElement, visibleClass);
+		addClass(footerElement, hiddenClass);
+	};
+}
+
+function unfixBarSetup (floatingElement, footerElement, visibleClass, hiddenClass) {
+	return () => {
+		removeClass(floatingElement, visibleClass);
+		removeClass(footerElement, hiddenClass);
+	};
+}
+
+// test whether we can store in local storage
+const canStore = () => {
+	const testKey = 'next-welcome:test-storage';
+	const testValue = 'can-store';
+	superstore.local.set(testKey, testValue);
+	const retrievedValue = superstore.local.get(testKey);
+	superstore.local.unset(testKey);
+	return (testValue === retrievedValue) && superstore.isPersisting();
+};
+
+function showWebAppLink(){
+	Array.from(document.querySelectorAll('.js-webapp-link')).forEach(link => {
+		link.querySelector('a').href = 'https://app.ft.com' + location.pathname + location.search;
+		addClass(link, 'visible');
+	});
+}
+
+function showAndroidLink(){
+	Array.from(document.querySelectorAll('.js-android-link')).forEach(link => {
+		addClass(link, 'visible');
+	});
+}
+
+function hideOptOutLink(){
+	Array.from(document.querySelectorAll('.js-optout-link')).forEach(link => {
+		addClass(link, 'hidden');
+	});
+}
+
+module.exports.init = () => {
+	let floatingElement = document.querySelector('.n-welcome--fixed');
+	let footerElement = document.querySelector('.n-welcome');
+	let storageKey = 'welcomePanelClosed';
+	let barPreviouslyHidden = Boolean(retrieve(storageKey));
+	let visibleClass = 'n-welcome--fixed-visible';
+	let hiddenClass = 'n-welcome--hidden';
+	let fixBar = fixBarSetup(floatingElement, footerElement, visibleClass, hiddenClass);
+	let unfixBar = unfixBarSetup(floatingElement, footerElement, visibleClass, hiddenClass);
+
+	if(isWebAppCapableDevice(navigator.userAgent)){
+		showWebAppLink();
+		hideOptOutLink();
+	}else if(isAndroidDevice(navigator.userAgent)){
+		showAndroidLink();
+		hideOptOutLink();
+	}
+
+	// Don't display the welcome bar if already acknowledged, or if we can't store in storage
+	if (!floatingElement || barPreviouslyHidden || !canStore()) {
+		return;
+	}
+
+	// otherwise fix the bar
+	let closeButton = document.querySelector('.n-welcome__close');
+	fixBar();
+	// so the bar only appears once
+	store(storageKey, 1);
+	closeButton.addEventListener('click', onButtonClose.bind(null, unfixBar, storageKey));
+};
