30814b80d95aca859fd5ddacd936473681887f94
Rhys Evans <rhys.evans@ft.com>
2016-06-20 11:31:51 +0100

    added middleware for serving n-ui bundle

---
+++ layout/middleware/index.js
@@ -0,0 +1,29 @@
+'use strict';
+const path = require('path');
+let version = false;
+let versionType = 'none';
+
+try {
+	version = require(path.join(process.cwd(), 'bower_components/n-ui/.bower.json')).version;
+
+	if (/(beta|rc)/.test(version)) {
+		versionType = 'beta';
+	} else {
+		versionType = 'semver';
+		version = version.split(".").slice(0,2).join('.');
+	}
+
+	version = 'v' + version;
+
+} catch (e) {}
+
+module.exports = function(req, res, next) {
+
+	if (versionType === 'semver' && res.locals.flags.nUiBundleMajorVersion) {
+		res.locals.nUiVersion = version[0].split('.').slice(0,1)[0];
+	} else {
+		res.locals.nUiVersion = version;
+	}
+
+	next()
+};
