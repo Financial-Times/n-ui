7a7faa6cd156c939c0c010b8dbf980168c405e6d
Matt Chadburn <matt.chadburn@ft.com>
2016-06-21 08:15:28 +0100

    Updated tools to v1.3.2

--- tracking/js/third-party/mouseflow.js
+++ tracking/js/third-party/mouseflow.js
@@ -8,20 +8,11 @@ const getAllocationId = function () {
 // Loads session cam tracking code
 module.exports = function (flags) {
 
-	const isSignUpApp = !!document.querySelector('html[data-next-app=signup]');
-
 	if (flags && (flags.get('mouseflowForce') || flags.get('mouseflow'))) {
 
 		var _mfq = _mfq || [];
-
-		(function() {
-			var mf = document.createElement("script");
-			mf.type = "text/javascript"; mf.async = true;
-			mf.src = "//cdn.mouseflow.com/projects/3d6fc486-2914-4efc-a5ae-35a5eac972f2.js";
-			document.getElementsByTagName("head")[0].appendChild(mf);
-		})();
-
 		_mfq.push(['setVariable', 'allocationId', getAllocationId()]);
+		loadScript('https://cdn.mouseflow.com/projects/3d6fc486-2914-4efc-a5ae-35a5eac972f2.js');
 
 	}
 }
