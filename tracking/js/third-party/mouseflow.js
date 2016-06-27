318ee0b178bf26fc8129a1a4d54cf9fe3eaa413c
Laura Carvajal <laura.carvajal@gmail.com>
2016-06-27 15:38:02 +0100

    spoorid

--- tracking/js/third-party/mouseflow.js
+++ tracking/js/third-party/mouseflow.js
@@ -1,7 +1,8 @@
 import {loadScript} from '../../../utils';
 
 const getCookieValue = function (key) {
-	const a = /${key}=([^;]+)/i.exec(document.cookie);
+	const regex = new RegExp(`${key}=([^;]+)`, 'i');
+	const a = regex.exec(document.cookie);
 	return (a) ? a[1] : undefined;
 }
 
@@ -9,11 +10,8 @@ const getCookieValue = function (key) {
 module.exports = function (flags) {
 
 	if (flags && (flags.get('mouseflowForce') || flags.get('mouseflow'))) {
-
 		var _mfq = _mfq || [];
-		_mfq.push(['setVariable', 'allocationId', getCookieValue('FTAllocation')]);
 		_mfq.push(['setVariable', 'spoorId', getCookieValue('spoor-id')]);
 		loadScript('https://cdn.mouseflow.com/projects/3d6fc486-2914-4efc-a5ae-35a5eac972f2.js');
-
 	}
 }
