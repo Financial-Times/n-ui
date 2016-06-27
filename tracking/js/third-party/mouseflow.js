bad9db61879784aa67f7ab2ae169d92ccbd24ed0
Laura Carvajal <laura.carvajal@gmail.com>
2016-06-27 14:49:14 +0100

    send spoorid to mouseflow

--- tracking/js/third-party/mouseflow.js
+++ tracking/js/third-party/mouseflow.js
@@ -1,7 +1,7 @@
 import {loadScript} from '../../../utils';
 
-const getAllocationId = function () {
-	const a = /FTAllocation=([^;]+)/i.exec(document.cookie);
+const getCookieValue = function (key) {
+	const a = /${key}=([^;]+)/i.exec(document.cookie);
 	return (a) ? a[1] : undefined;
 }
 
@@ -11,7 +11,8 @@ module.exports = function (flags) {
 	if (flags && (flags.get('mouseflowForce') || flags.get('mouseflow'))) {
 
 		var _mfq = _mfq || [];
-		_mfq.push(['setVariable', 'allocationId', getAllocationId()]);
+		_mfq.push(['setVariable', 'allocationId', getCookieValue('FTAllocation')]);
+		_mfq.push(['setVariable', 'spoorId', getCookieValue('spoor-id')]);
 		loadScript('https://cdn.mouseflow.com/projects/3d6fc486-2914-4efc-a5ae-35a5eac972f2.js');
 
 	}
