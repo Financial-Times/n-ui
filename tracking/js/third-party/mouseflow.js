f8082ab6ab2fa2f3c6fa80cc9aa58ea68e85d5d2
Laura Carvajal <laura.carvajal@gmail.com>
2016-06-28 09:19:11 +0100

    spoorid (#163)

--- tracking/js/third-party/mouseflow.js
+++ tracking/js/third-party/mouseflow.js
@@ -11,8 +11,8 @@ module.exports = function (flags) {
 
 	if (flags && (flags.get('mouseflowForce') || flags.get('mouseflow'))) {
 
-		const _mfq = _mfq || [];
-		_mfq.push(['setVariable', 'spoorId', getCookieValue('spoor-id')]);
+		window._mfq = window._mfq || [];
+		window._mfq.push(['setVariable', 'spoorId', getCookieValue('spoor-id')]);
 		loadScript('https://cdn.mouseflow.com/projects/3d6fc486-2914-4efc-a5ae-35a5eac972f2.js');
 
 	}
