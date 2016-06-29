1bf6b835139fdbe090672971666eb613be2e43cd
ifeanyiisitor <ifeanyiisitor@gmail.com>
2016-06-29 09:58:40 +0100

    stubbing init function to prevent error

--- promo-messages/index.js
+++ promo-messages/index.js
@@ -1,1 +1,4 @@
 //module.exports = require('n-messages'); // FIXME: Commenting this out for now to fix preact issue as it is not currently being used
+module.exports = { // FIXME: This is to prevent a browser error for now until this whle thing is sorted out
+	init: function () {}
+};
