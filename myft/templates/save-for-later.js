4d0c473bc27adcf9cfafd9d4a1f399d5b34fe729
Ben Fletcher <ben.fletcher@ft.com>
2016-06-30 15:36:26 +0100

    clearer indention

--- myft/templates/save-for-later.js
+++ myft/templates/save-for-later.js
@@ -3,18 +3,18 @@ import React from 'react';
 export default ({ myFtApiWrite, contentId, variant, buttonText }) => {
 	return !myFtApiWrite ? null : (
 		<form className="n-myft-ui n-myft-ui--save" method="POST"
-			data-content-id={contentId}
-			data-myft-ui="saved"
-			action={`/__myft/api/core/saved/content/${contentId}?method=put`}>
-			<button
-				type="submit"
-				className={`n-myft-ui__button${variant ? 'n-myft-ui__button--' + variant : ''}`}
-				data-trackable="save-for-later"
-				data-alternate-label="Saved to myFT"
-				data-alternate-text="Saved"
 				data-content-id={contentId}
-				aria-label="Save to myFT"
-				title="Save to myFT"
+				data-myft-ui="saved"
+				action={`/__myft/api/core/saved/content/${contentId}?method=put`}>
+			<button
+					type="submit"
+					className={`n-myft-ui__button${variant ? 'n-myft-ui__button--' + variant : ''}`}
+					data-trackable="save-for-later"
+					data-alternate-label="Saved to myFT"
+					data-alternate-text="Saved"
+					data-content-id={contentId}
+					aria-label="Save to myFT"
+					title="Save to myFT"
 			>{buttonText || 'Save'}</button>
 		</form>
 	)
