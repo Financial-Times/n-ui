4bd1abb90edddec1d3f091c7ff3557f68e07954a
Rhys Evans <wheresrhys@gmail.com>
2016-07-04 15:04:55 +0100

    Rhys/test deploy (#197)
    * able to make dummy-releases
    
    * verbose deploy

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -2,13 +2,15 @@
 const shellpromise = require('shellpromise');
 let versions = [require('../bower_components/n-ui/.bower.json').version];
 
-if (!/(beta|rc)/.test(versions[0])) {
+if (!versions[0]) {
+	versions = ['dummy-release'];
+} else if (!/(beta|rc)/.test(versions[0])) {
 	versions[0] = versions[0].split('.').slice(0,2).join('.');
 	versions.push(versions[0].split('.').slice(0,1)[0]);
 }
 
 Promise.all(versions.map(version => {
-	return shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/v${version}/ --strip 1 --bucket ft-next-n-ui-prod --cache-control 'must-revalidate, max-age=3600' --monitor`)
+	return shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/v${version}/ --strip 1 --bucket ft-next-n-ui-prod --cache-control 'must-revalidate, max-age=3600' --monitor`, {verbose: true})
 		.catch(err => {
 			console.error(err)
 			process.exit(2);
