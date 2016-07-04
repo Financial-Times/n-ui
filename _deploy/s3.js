f0c522068418cb819176b343b09a0065e05c6d59
Rhys Evans <rhys.evans@ft.com>
2016-07-04 12:56:29 +0100

    experiment to monitor file size

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -1,12 +1,13 @@
 'use strict';
 const shellpromise = require('shellpromise');
 let versions = [require('../bower_components/n-ui/.bower.json').version];
-
+console.log(versions);
 if (!/(beta|rc)/.test(versions[0])) {
 	versions[0] = versions[0].split('.').slice(0,2).join('.');
 	versions.push(versions[0].split('.').slice(0,1)[0]);
 }
 
 Promise.all(versions.map(version => {
-	return shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/v${version}/ --strip 1 --bucket ft-next-n-ui-prod --cache-control 'must-revalidate, max-age=3600'`);
+	return shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/test-v${version}/ --strip 1 --bucket ft-next-n-ui-prod --cache-control 'must-revalidate, max-age=3600' --monitor`)
+	.catch(console.log.bind(console))
 }));
