035e56f64d6a61537cbc8ea7ed30aab66c349a4e
Rhys Evans <rhys.evans@ft.com>
2016-07-04 14:41:16 +0100

    able to make dummy-releases

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -2,7 +2,9 @@
 const shellpromise = require('shellpromise');
 let versions = [require('../bower_components/n-ui/.bower.json').version];
 
-if (!/(beta|rc)/.test(versions[0])) {
+if (!versions[0]) {
+	versions = ['dummy-release'];
+} else if (!/(beta|rc)/.test(versions[0])) {
 	versions[0] = versions[0].split('.').slice(0,2).join('.');
 	versions.push(versions[0].split('.').slice(0,1)[0]);
 }
