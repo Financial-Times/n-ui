583bde8b2ce8c0ef2eb69887576514594d3f99ee
Simon Legg <leggsimon@gmail.com>
2016-07-06 14:17:47 +0100

    include typeahead and init o-header

--- header/index.js
+++ header/index.js
@@ -1,16 +1,25 @@
+const Typeahead = require('./typeahead');
+
 function init (flags) {
-	require('n-header-footer/header_new').init(flags);
+	require('o-header').init(flags);
 
 	if (flags.get('fancyDrawer')) {
 		require('n-header-footer/drawer').init();
 	}
 
-	if (flags.get('meganav')) {
-		require('n-header-footer/meganav').init();
-	}
-
 	if (flags.get('sectionBreadcrumbs')) {
 		require('n-header-footer/breadcrumb').init();
+	}
+
+	// TODO: refactor into for...loop
+	const typeaheadEls = [...document.querySelectorAll('[data-typeahead]')];
+	if (flags.get('typeahead') && typeaheadEls.length) {
+		typeaheadEls.forEach(function (element) {
+			new Typeahead(
+				element,
+				'//' + window.location.host + '/search-suggestions?q='
+			);
+		});
 	}
 }
 
