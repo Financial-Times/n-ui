c42a2961d312ae6c55444671a692ef1e546df92c
Rhys Evans <rhys.evans@ft.com>
2016-08-08 12:29:13 +0100

    deploy a shared head.css bundle

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -8,7 +8,6 @@ if (!versions[0]) {
 	versions[0] = versions[0].split('.').slice(0,2).join('.');
 	versions.push(versions[0].split('.').slice(0,1)[0]);
 }
-
 Promise.all(versions.map(version => {
 	return shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/no-cache/v${version}/ \\
 		--strip 1 --bucket ft-next-n-ui-prod \\
