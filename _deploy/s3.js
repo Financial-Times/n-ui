8803be7012fb8fdb235cc4121bbb05c3409c48ce
Rhys Evans <wheresrhys@gmail.com>
2016-08-18 15:58:43 +0100

    fixed monitoring of buit asset sizes (#350)
    * fixed monitoring of buit asset sizes
    
    * Updated tools to v1.4.4
    
    * try tweka of makefile

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -8,11 +8,12 @@ if (!versions[0]) {
 	versions[0] = versions[0].split('.').slice(0,2).join('.');
 	versions.push(versions[0].split('.').slice(0,1)[0]);
 }
-Promise.all(versions.map(version => {
+
+Promise.all(versions.map((version, i) => {
 	return shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/no-cache/v${version}/ \\
 		--strip 1 --bucket ft-next-n-ui-prod \\
 		--surrogate-control 'must-revalidate, max-age=3600, stale-while-revalidate=60, stale-on-error=86400' \\
-		--cache-control 'no-cache, must-revalidate, max-age=3600' --monitor`,
+		--cache-control 'no-cache, must-revalidate, max-age=3600'${i === 0 ? ' --monitor' : ''}`,
 	{verbose: true})
 		.catch(err => {
 			console.error(err)
