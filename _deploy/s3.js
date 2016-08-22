103c433ec00607582e0be3bff52ff8812a6cec24
Rhys Evans <wheresrhys@gmail.com>
2016-08-22 13:50:02 +0100

    releasing non-semver tags

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -1,20 +1,25 @@
 'use strict';
 const shellpromise = require('shellpromise');
-let versions = [require('../bower_components/n-ui/.bower.json').version];
+const semver = require('semver');
+const tag = process.env.CIRCLE_TAG;
+let versions;
 
-if (!versions[0]) {
+if (!tag) {
 	versions = ['dummy-release'];
-} else if (!/(beta|rc)/.test(versions[0])) {
-	versions[0] = versions[0].split('.').slice(0,2).join('.');
-	versions.push(versions[0].split('.').slice(0,1)[0]);
+} else if (!semver.valid(tag) || /(beta|rc)/.test(tag)) {
+	versions = [tag];
+} else {
+	versions = [
+		tag.split('.').slice(0,2).join('.'),
+		tag.split('.').slice(0,1).join('.')
+	]
 }
 
 Promise.all(versions.map((version, i) => {
 	return shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/no-cache/v${version}/ \\
 		--strip 1 --bucket ft-next-n-ui-prod \\
 		--surrogate-control 'must-revalidate, max-age=3600, stale-while-revalidate=60, stale-on-error=86400' \\
-		--cache-control 'no-cache, must-revalidate, max-age=3600'${i === 0 ? ' --monitor' : ''}`,
-	{verbose: true})
+		--cache-control 'no-cache, must-revalidate, max-age=3600'${i === 0 ? ' --monitor' : ''}`, {verbose: true})
 		.catch(err => {
 			console.error(err)
 			process.exit(2);
