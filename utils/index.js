fd1253f27926fc6831980f37a25dc2395254d36e
Rhys Evans <wheresrhys@gmail.com>
2016-08-26 14:03:53 +0100

    integrated LUX performance tracking (#377)

--- utils/index.js
+++ utils/index.js
@@ -55,7 +55,7 @@ module.exports = {
 		rootEl.dispatchEvent(event);
 	},
 	perfMark: name => {
-		const performance = window.performance || window.msPerformance || window.webkitPerformance || window.mozPerformance;
+		const performance = window.LUX || window.performance || window.msPerformance || window.webkitPerformance || window.mozPerformance;
 		if (performance && performance.mark) {
 			performance.mark(name);
 		}
