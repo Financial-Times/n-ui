c846607d02873fcbf6c880e6b4cc79d8020539a1
Rhys Evans <rhys.evans@ft.com>
2016-09-02 15:36:47 +0100

    moving welcome message and footer our of critical css

--- _test-server/app.js
+++ _test-server/app.js
@@ -9,6 +9,7 @@ const app = module.exports = express({
 	withHandlebars: true,
 	withNavigation: true,
 	withAnonMiddleware: true,
+	hasHeadCss: true,
 	layoutsDir: path.join(process.cwd(), '/layout'),
 	viewsDirectory: '/_test-server/views',
 	directory: process.cwd()
@@ -24,23 +25,26 @@ app.listen(5005)
 	.then(app => {
 
 		console.log('Demo app up and running on port 5005');
-		//generate a test page and send it to S3
-		fetch('http://localhost:5005/', {
-			headers: {
-				'FT-Flags': 'ads:off'
-			}
-		})
-			.then(res => res.text())
-			.then(text => fs.writeFileSync(path.join(process.cwd(), 'test-page.html'), text))
-			.then(() => app.close())
-			.then(() => {
-				return shellpromise(`nht deploy-static test-page.html public/main.css public/main.js public/main.css.map public/main.js.map --destination n-ui/test-page/${process.env.CIRCLE_BUILD_NUM}/ \\
-					--bucket ft-next-n-ui-prod \\
-					--cache-control 'no-cache, must-revalidate'`, {verbose: true})
-					.then(() => process.exit(0))
-					.catch(err => {
-						console.error(err)
-						process.exit(2);
-					});
+		// in CI generate a test page and send it to S3
+		if (process.env.CI) {
+
+			fetch('http://localhost:5005/', {
+				headers: {
+					'FT-Flags': 'ads:off'
+				}
 			})
+				.then(res => res.text())
+				.then(text => fs.writeFileSync(path.join(process.cwd(), 'test-page.html'), text))
+				.then(() => app.close())
+				.then(() => {
+					return shellpromise(`nht deploy-static test-page.html public/main.css public/main.js public/main.css.map public/main.js.map --destination n-ui/test-page/${process.env.CIRCLE_BUILD_NUM}/ \\
+						--bucket ft-next-n-ui-prod \\
+						--cache-control 'no-cache, must-revalidate'`, {verbose: true})
+						.then(() => process.exit(0))
+						.catch(err => {
+							console.error(err)
+							process.exit(2);
+						});
+				})
+		}
 	});
--- webpack.config.test.js
+++ webpack.config.test.js
@@ -4,6 +4,7 @@ const nWebpack = require('@financial-times/n-webpack');
 
 module.exports = nWebpack({
 	withBabelPolyfills: true,
+	withHeadCss: true,
 	entry: {
 		'./public/main.js': './_test-server/client/main.js',
 		'./public/main.css': './_test-server/client/main.scss'
