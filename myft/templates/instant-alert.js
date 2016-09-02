cc7c4d8f992fd31cc058b315c5f9c8ac633be748
Dawn Budge <dawn.budge@ft.com>
2016-09-02 16:46:00 +0100

    Better text handling on instant alert

--- myft/templates/instant-alert.js
+++ myft/templates/instant-alert.js
@@ -5,9 +5,8 @@ import UpdateMyFtForm from './update-myft-form';
 export default class InstantAlert extends Component {
 	render () {
 		let attrs = Object.assign({}, this.props);
-		const buttonText = this.props.buttonText || 'Instant alerts off';
-		const alternateText = this.props.alternateText || this.props.buttonText || 'Instant alerts on';
-		const startButtonText = (this.props.hasInstantAlert) ? alternateText : buttonText;
+		const isOnText = this.props.buttonText || 'Instant alerts on';
+		const isOffText = this.props.alternateText || this.props.buttonText || 'Instant alerts off';
 		const instantAttrs = {
 			classes: classNames('n-myft-ui--instant', this.props.classes),
 			action: '/__myft/api/core/followed/concept/' + this.props.conceptId + '?method=put',
@@ -16,8 +15,8 @@ export default class InstantAlert extends Component {
 			trackable: 'instant',
 			activateLabel: 'Get instant alerts for ',
 			deactivateLabel: 'Stop instant alerts for ',
-			buttonText: startButtonText,
-			alternateText,
+			buttonText: (this.props.hasInstantAlert) ? isOnText : isOffText,
+			alternateText: (this.props.hasInstantAlert) ? isOffText: isOnText,
 			btnName: '_rel.instant',
 			btnValue: !this.props.hasInstantAlert
 
