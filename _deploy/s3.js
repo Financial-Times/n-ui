733156de0d7d393ee15f3455f7aa56192433dcf5
Rhys Evans <rhys.evans@ft.com>
2016-09-05 10:35:03 +0100

    more improvements to deployment

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -2,51 +2,60 @@
 const shellpromise = require('shellpromise');
 const semver = require('semver');
 const fetch = require('node-fetch');
-
+const deployStatic = require('@financial-times/n-heroku-tools').deployStatic.task;
 const tag = process.env.CIRCLE_TAG;
 let versions;
+let isOfficialRelease = false;
 
 if (!tag) {
 	versions = ['dummy-release'];
 } else if (!semver.valid(tag) || /(beta|rc)/.test(tag)) {
 	versions = [tag];
 } else {
+	isOfficialRelease = true;
 	versions = [
 		tag.split('.').slice(0,2).join('.'),
 		tag.split('.').slice(0,1).join('.')
 	]
 }
 
-Promise.all(versions.map((version, i) => {
-	return shellpromise(`nht deploy-static \`find . -path "./dist/*"\` --destination n-ui/no-cache/${version}/ \\
-		--strip 1 --bucket ft-next-n-ui-prod \\
-		--surrogate-control 'must-revalidate, max-age=3600, stale-while-revalidate=60, stale-on-error=86400' \\
-		--cache-control 'no-cache, must-revalidate, max-age=3600'${i === 0 ? ' --monitor' : ''}`, {verbose: true})
-		.then(() => {
-			return shellpromise('find . -path "./dist/*"')
-				.then(files => files.split('\n')
-					.map(file => {
-						return fetch(`https://next-geebee.ft.com/n-ui/no-cache/${version}/${file.split('/').pop()}`, {
-							method: 'PURGE',
-							headers: {
-								'Fastly-Soft-Purge': 1,
-								'Fastly-Key': process.env.FASTLY_API_KEY
-							}
-						})
-						.then(res => {
-							if(!res.ok) {
-								throw new Error(`failed to purge ${file}`)
-							}
-						})
-						.catch(err => {
-							console.error(err);
-						})
-					})
-				);
-		})
-		.catch(err => {
-			console.error(err)
-			process.exit(2);
-		});
+shellpromise('find . -path "./dist/*"')
+	.then(files => {
 
-}));
+		files = files.split('\n').filter(f => !!f);
+
+		return Promise.all(versions.map((version, i) => {
+			return deployStatic({
+				files: files,
+				destination: `n-ui/no-cache/${version}`,
+				bucket: 'ft-next-n-ui-prod',
+				strip: 1,
+				monitor: isOfficialRelease && i === 0,
+				cacheControl: 'no-cache, must-revalidate, max-age=3600',
+				surrogateControl: 'must-revalidate, max-age=3600, stale-while-revalidate=60, stale-on-error=86400'
+			})
+				.then(() => files.map(file => {
+					return fetch(`https://next-geebee.ft.com/n-ui/no-cache/${version}/${file.split('/').pop()}`, {
+						method: 'PURGE',
+						headers: {
+							'Fastly-Soft-Purge': 1,
+							'Fastly-Key': process.env.FASTLY_API_KEY
+						}
+					})
+					.then(res => {
+						if(!res.ok) {
+							throw new Error(`failed to purge ${file}`)
+						}
+					})
+					.catch(err => {
+						console.error(err);
+					})
+				}))
+				.catch(err => {
+					console.error(err)
+					process.exit(2);
+				});
+
+		}));
+	})
+	.catch(console.log.bind(console))
--- _test-server/app.js
+++ _test-server/app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const express = require('@financial-times/n-express');
 const path = require('path');
-const shellpromise = require('shellpromise');
+const deployStatic = require('@financial-times/n-heroku-tools').deployStatic.task;
 
 const app = module.exports = express({
 	name: process.env.CIRCLE_BUILD_NUM ? `n-ui/test-page/${process.env.CIRCLE_BUILD_NUM}/public` : 'public',
@@ -23,10 +23,8 @@ app.get('/', (req, res) => {
 
 app.listen(5005)
 	.then(app => {
-
-		console.log('Demo app up and running on port 5005');
 		// in CI generate a test page and send it to S3
-		if (process.env.CI) {
+		if (process.env.CIRCLE_BUILD_NUM) {
 
 			fetch('http://localhost:5005/', {
 				headers: {
@@ -37,10 +35,12 @@ app.listen(5005)
 				.then(text => fs.writeFileSync(path.join(process.cwd(), 'test-page.html'), text))
 				.then(() => app.close())
 				.then(() => {
-					return shellpromise(`nht deploy-static test-page.html public/main.css public/main.js public/main.css.map public/main.js.map --destination n-ui/test-page/${process.env.CIRCLE_BUILD_NUM}/ \\
-						--bucket ft-next-n-ui-prod \\
-						--cache-control 'no-cache, must-revalidate'`, {verbose: true})
-						.then(() => process.exit(0))
+					return deployStatic({
+						files: ['test-page.html', 'public/main.css', 'public/main.js', 'public/main.css.map', 'public/main.js.map'],
+						destination: `n-ui/test-page/${process.env.CIRCLE_BUILD_NUM}/`,
+						bucket: 'ft-next-n-ui-prod',
+						cacheControl: 'no-cache, must-revalidate',
+					})
 						.catch(err => {
 							console.error(err)
 							process.exit(2);
