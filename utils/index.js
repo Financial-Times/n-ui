aca52c4f28c99a268e3547f29fa0c6bc9c5fdbf6
Rhys Evans <rhys.evans@ft.com>
2016-09-05 10:09:31 +0100

    more byte-saving

--- utils/index.js
+++ utils/index.js
@@ -29,7 +29,7 @@ module.exports = {
 			timeout = setTimeout(later, wait);
 		};
 	},
-	uuid: require('./js/uuid'),
+	uuid: function b (a){return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,b)},
 	loadScript: (src) => {
 		return new Promise((res, rej) => {
 			const script = window.ftNextLoadScript(src);
