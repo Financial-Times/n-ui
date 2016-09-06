e3ca168019841c49a38e1f665f81d12b46196f5b
Maggie Allen <maggie.allen@ft.com>
2016-09-06 17:57:17 +0100

    cta button works and gets disabled when done

--- my-ft-digest-promo/index.js
+++ my-ft-digest-promo/index.js
@@ -1,11 +1,42 @@
 const myFtClient = require('next-myft-client');
+const buttons = require('../buttons');
+let btn;
 
+function init(){
+  console.log("Hello World");
+  btn = document.querySelector(".n-myft-digest-promo__cta-btn");
+  const conceptId = btn.getAttribute('data-concept-id');
+  const metaConcept = {
+    name: btn.getAttribute('data-title'),
+    taxonomy: btn.getAttribute('data-taxonomy')
+  };
+  const metaEmail = {
+    _rel: {
+      type: "daily",
+      timezone: "Europe/London"
+    }
+  };
+
+
+  shouldShowPromo(conceptId).then(shouldShow => {
+
+    console.log('SHOULD SHOW PROMO', shouldShow);
+
+    if(shouldShow) {
+      showPromo();
+      btn.addEventListener('click', function(){
+        addToDigest(conceptId, metaConcept, metaEmail);
+      }, false);
+    }
+  });
+};
 
 function shouldShowPromo(conceptId) {
   return Promise.all([
     myFtClient.get('followed', 'concept', conceptId),
     myFtClient.get('preferred', 'preference', 'email-digest')
   ]).then(values => {
+    console.log('MYFT DATA', values);
     return values[0].length === 0 && values[1].length === 0;
   });
 }
@@ -15,28 +46,26 @@ function showPromo() {
   element.classList.add("n-myft-digest-promo--enabled");
 }
 
-function addToDigest() {
-  console.log('CLICKED');
-
-}
-
-function init(){
-  const btn = document.querySelector(".n-myft-digest-promo__cta-btn");
-  const conceptId = btn.getAttribute('data-concept-id')
-
-  shouldShowPromo(conceptId).then(shouldShow => {
-    if(shouldShow) {
-      showPromo();
-      btn.addEventListener('click', addToDigest);
-    }
+function addToDigest(conceptId, metaConcept, metaEmail) {
+  console.log('CLICKED', metaConcept, metaEmail);
+  return Promise.all([
+    myFtClient.add('user', null, 'followed', 'concept', conceptId, metaConcept),
+    myFtClient.add('user', null, 'preferred', 'preference', 'email-digest', metaEmail)
+  ]).then(values => {
+    console.log('values:', values);
+    buttons.toggleState(btn, true);
+    btn.setAttribute('disabled', true);
+    console.log(btn.getAttribute('disabled'));
+    //supress confirm-subscribed overlay
+  }).catch(err => {
+    console.log(err);
   });
-};
+}
 
 module.exports = { init };
 
 //init function - check for signed in status & digest preference status
 //then display css (add the class to the promo)
-//poss use jQuery $("n-myft-digest-promo").addClass("n-myft-digest-promo--enabled");
 //set up button event handlers
 
 //onclick listener - send put requests to two routes using next-myft-client
