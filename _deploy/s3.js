fd7abae1bc158a94ddcd347bd876678c9b7103c8
Rhys Evans <wheresrhys@gmail.com>
2016-09-08 18:58:22 +0100

    20 minute cache

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -27,12 +27,12 @@ shellpromise('find . -path "./dist/*"')
 			return arr.concat([{
 				version,
 				monitor: i === 0, // only monitor the size of the first copy deployed
-				cacheControl: 'no-cache, must-revalidate, max-age=3600',
+				cacheControl: 'no-cache, must-revalidate, max-age=1200',
 				directory: 'no-cache'
 			}, {
 				version,
 				monitor: false,
-				cacheControl: 'must-revalidate, max-age=3600',
+				cacheControl: 'must-revalidate, max-age=1200',
 				directory: 'cached'
 			}])
 		}, []);
