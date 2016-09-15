61bd0c5d5437d3d5fb8faa845c0563101126d4ef
Laurie Boyes <laurie.boyes@gmail.com>
2016-09-15 09:53:05 +0100

    tour tip set attrs as data, not as data.content

--- tour-tip/component.js
+++ tour-tip/component.js
@@ -5,7 +5,7 @@ import { buildImageServiceUrl } from '@financial-times/n-image/src/helpers';
 
 export default class TourTip extends Component {
 	render () {
-		let attrs = Object.assign({}, this.props.data.content);
+		let attrs = Object.assign({}, this.props.data);
 
 		const classes = {
 			'tour-tip': true,
