2bea6048e8762acce87cf1ae393840ca315a91a0
Keith Rogers <git@keithrogers.eu>
2016-09-20 11:00:28 +0100

    Rename attrs to data
    It better represents its usage

--- tour-tip/component.js
+++ tour-tip/component.js
@@ -5,40 +5,40 @@ import { buildImageServiceUrl } from '@financial-times/n-image/src/helpers';
 
 export default class TourTip extends Component {
 	render () {
-		let attrs = JSON.parse(JSON.stringify(this.props.data));
+		let data = JSON.parse(JSON.stringify(this.props.data));
 
-		const modifiers = [`tour-tip--${attrs.settings.size}`, ...(attrs.modifiers || []) ];
+		const modifiers = [`tour-tip--${data.settings.size}`, ...(data.modifiers || []) ];
 		const classes = classNames({
 			'tour-tip': true
 		}, modifiers);
 
 		const createMarkup = (prop) => ({__html: prop});
 
-		const tourLink = (attrs.settings.hasTourPageLink) ?
+		const tourLink = (data.settings.hasTourPageLink) ?
 			(<p className="tour-tip__link"><a href="/tour" className="tour-tip__link" data-trackable="tour-link">New hints and tips</a></p>)
 			: null;
 
-		const body = (attrs.content.body) ?
-			(<p className="tour-tip__body" dangerouslySetInnerHTML={createMarkup(attrs.content.body)}></p>) : null;
+		const body = (data.content.body) ?
+			(<p className="tour-tip__body" dangerouslySetInnerHTML={createMarkup(data.content.body)}></p>) : null;
 
-		const cta = (attrs.content.ctaUrl && attrs.content.ctaLabel) ?
-			(<a href={attrs.content.ctaUrl} className="tour-tip__cta o-buttons o-buttons--standout" data-trackable="cta">{attrs.content.ctaLabel}</a>)
+		const cta = (data.content.ctaUrl && data.content.ctaLabel) ?
+			(<a href={data.content.ctaUrl} className="tour-tip__cta o-buttons o-buttons--standout" data-trackable="cta">{data.content.ctaLabel}</a>)
 			: null;
 
-		const image = (attrs.content.imageUrl) ?
+		const image = (data.content.imageUrl) ?
 			(<div className="tour-tip__img-container">
 				<Image
-					src={buildImageServiceUrl(attrs.content.imageUrl, (attrs.content.imageUrl.substr(-4,4) === '.svg') ? {format: 'svg'} : {} )}
-					alt={attrs.content.imageAlt}
+					src={buildImageServiceUrl(data.content.imageUrl, (data.content.imageUrl.substr(-4,4) === '.svg') ? {format: 'svg'} : {} )}
+					alt={data.content.imageAlt}
 					classes={['tour-tip__img']}
 				/>
 			</div>) : null;
 
-		return <div className={classes} data-trackable={`tour-tip-${attrs.id}`}>
+		return <div className={classes} data-trackable={`tour-tip-${data.id}`}>
 			<div className="tour-tip__main">
 				<div className="tour-tip__text">
 					{tourLink}
-					<h2 className="tour-tip__standout">{attrs.content.title}</h2>
+					<h2 className="tour-tip__standout">{data.content.title}</h2>
 					{body}
 					{cta}
 				</div>
