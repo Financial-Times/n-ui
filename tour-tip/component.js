ef830d67e4513c18ca11eaab70378222ce4aef50
Keith Rogers <git@keithrogers.eu>
2016-09-20 11:50:49 +0100

    Move conditional items into their own render functions
    Saw this in one of @debugwand's files and it made a lot of sense :-)

--- tour-tip/component.js
+++ tour-tip/component.js
@@ -4,45 +4,56 @@ import { Image } from '@financial-times/n-image';
 import { buildImageServiceUrl } from '@financial-times/n-image/src/helpers';
 
 export default class TourTip extends Component {
-	render () {
-		let data = JSON.parse(JSON.stringify(this.props.data));
 
-		const modifiers = [`tour-tip--${data.settings.size}`, ...(data.modifiers || []) ];
-		const classes = classNames({
-			'tour-tip': true
-		}, modifiers);
+	renderTourLink (data) {
+		if (data.settings.hasTourPageLink) {
+			return <p className="tour-tip__link"><a href="/tour" className="tour-tip__link" data-trackable="tour-link">New hints and tips</a></p>;
+		}
+	}
 
-		const createMarkup = (prop) => ({__html: prop});
+	renderBody (data) {
+		const createMarkup = (prop) => ({ __html: prop });
 
-		const tourLink = (data.settings.hasTourPageLink) ?
-			(<p className="tour-tip__link"><a href="/tour" className="tour-tip__link" data-trackable="tour-link">New hints and tips</a></p>)
-			: null;
+		if (data.content.body) {
+			return <p className="tour-tip__body" dangerouslySetInnerHTML={createMarkup(data.content.body)}></p>;
+		}
+	}
 
-		const body = (data.content.body) ?
-			(<p className="tour-tip__body" dangerouslySetInnerHTML={createMarkup(data.content.body)}></p>) : null;
+	renderCta (data) {
+		if (data.content.ctaUrl && data.content.ctaLabel) {
+			return <a href={data.content.ctaUrl} className="tour-tip__cta o-buttons o-buttons--standout" data-trackable="cta">{data.content.ctaLabel}</a>;
+		}
+	}
 
-		const cta = (data.content.ctaUrl && data.content.ctaLabel) ?
-			(<a href={data.content.ctaUrl} className="tour-tip__cta o-buttons o-buttons--standout" data-trackable="cta">{data.content.ctaLabel}</a>)
-			: null;
+	renderImage (data) {
+		if (data.content.imageUrl) {
 
-		const image = (data.content.imageUrl) ?
-			(<div className="tour-tip__img-container">
+			return <div className="tour-tip__img-container">
 				<Image
 					src={buildImageServiceUrl(data.content.imageUrl, (data.content.imageUrl.substr(-4,4) === '.svg') ? {format: 'svg'} : {} )}
 					alt={data.content.imageAlt}
 					classes={['tour-tip__img']}
 				/>
-			</div>) : null;
+			</div>;
+		}
+	}
+
+	render () {
+		let data = JSON.parse(JSON.stringify(this.props.data));
+		const modifiers = [`tour-tip--${data.settings.size}`, ...(data.modifiers || []) ];
+		const classes = classNames({
+			'tour-tip': true
+		}, modifiers);
 
 		return <div className={classes} data-trackable={`tour-tip-${data.id}`}>
 			<div className="tour-tip__main">
 				<div className="tour-tip__text">
-					{tourLink}
+					{this.renderTourLink(data)}
 					<h2 className="tour-tip__standout">{data.content.title}</h2>
-					{body}
-					{cta}
+					{this.renderBody(data)}
+					{this.renderCta(data)}
 				</div>
-				{image}
+				{this.renderImage(data)}
 			</div>
 		</div>
 	}
