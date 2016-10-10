f247df2a940731788b4dfe01f53e667d00ce3320
Laura Carvajal <laura.carvajal@gmail.com>
2016-10-10 14:17:08 +0100

    screen sizes

--- .pa11yci.js
+++ .pa11yci.js
@@ -1,17 +1,38 @@
 module.exports = {
-	"defaults": {
-		"timeout": 1000,
-		"page": {
-			"viewport": {
-				"width": 320,
-				"height": 480
-			}
-		}
-	},
 	"urls": [
 		{
 			"url": "https://n-ui.ft.com/n-ui/test-page/" + process.env.CIRCLE_BUILD_NUM + "/test-page.html",
 			"timeout": 10000
+		},
+		{
+			"url": "https://n-ui.ft.com/n-ui/test-page/" + process.env.CIRCLE_BUILD_NUM + "/test-page.html",
+			"timeout": 10000,
+			"page": {
+				"viewport": {
+					"width": 768,
+					"height": 1024
+				}
+			}
+		},
+		{
+			"url": "https://n-ui.ft.com/n-ui/test-page/" + process.env.CIRCLE_BUILD_NUM + "/test-page.html",
+			"timeout": 10000,
+			"page": {
+				"viewport": {
+					"width": 490,
+					"height": 732
+				}
+			}
+		},
+		{
+			"url": "https://n-ui.ft.com/n-ui/test-page/" + process.env.CIRCLE_BUILD_NUM + "/test-page.html",
+			"timeout": 10000,
+			"page": {
+				"viewport": {
+					"width": 320,
+					"height": 480
+				}
+			}
 		}
 	]
 }
