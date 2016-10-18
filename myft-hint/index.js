2199f7244efa9547d2a9367d742c2168b3b28371
Maggie Allen <maggie.allen@ft.com>
2016-10-18 10:22:22 +0100

    linting clean up

--- myft-hint/index.js
+++ myft-hint/index.js
@@ -1,14 +1,22 @@
 
 
-function shouldShowHint (conceptId){
+function shouldShowHint (){
 	return Promise.all([
 		//flag on
 		//signed-in user - myFtClient
- 		//show 3 times
+		//show 3 times
 		//stream and article pages
 ])};
 
 function showHint () {
-	hint = document.querySelector('.myft-hint');
+	let hint = document.querySelector('.myft-hint');
 	hint.classList.add('myft-hint__show');
 };
+
+function init () {
+	if(shouldShowHint) {
+		showHint();
+	}
+};
+
+module.exports = { init };
