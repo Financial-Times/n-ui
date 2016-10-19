f8fea0b60c0373f98c941b00622a0b7972ba0247
Maggie Allen <maggie.allen@ft.com>
2016-10-19 17:23:33 +0100

    display logic and css styling added

--- myft-hint/index.js
+++ myft-hint/index.js
@@ -1,22 +1,37 @@
-
+const hint = document.querySelector('.myft-hint');
+const superstore = require('superstore-sync');
+const STORAGE_KEY = 'myft-hint-shown-count';
+const MAX_SHOWS = 3
 
 function shouldShowHint (){
-	return Promise.all([
-		//flag on - check in /header/top.html
-		//not anonymous - check in /header/top.html
-		//signed-in user - myFtClient
-		//show 3 times
+	return getHintShownCount() <= MAX_SHOWS;
 		//stream and article pages - flag, regex,- check url path for [/^\/(stream.*)|(article.*)/i]
-])};
+};
 
 function showHint () {
-	let hint = document.querySelector('.myft-hint');
 	hint.classList.add('myft-hint__show');
 };
 
+function getHintShownCount () {
+	return superstore.local.get(STORAGE_KEY);
+}
+
+function setHintShownCount (count) {
+	superstore.local.set(STORAGE_KEY, count);
+}
+
+function incrementHintShownCount () {
+	//if no STORAGE_KEY, setHintSHownCount
+	let count = getHintShownCount();
+	setHintShownCount(count++);
+	// superstore.session.set(STORAGE_KEY++);
+}
+
 function init () {
+	if(!superstore.isPersisting() || !hint) { return; }
 	if(shouldShowHint) {
 		showHint();
+		incrementHintShownCount();
 	}
 };
 
