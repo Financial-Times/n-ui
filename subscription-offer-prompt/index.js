fbe659368c7cdc6eb197b261bbb3fc22c2d27eb5
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-10-21 15:20:20 +0100

    Don't show slider if TLS < 1.1

--- subscription-offer-prompt/index.js
+++ subscription-offer-prompt/index.js
@@ -18,14 +18,25 @@ const getPromptLastClosed = () => promptLastSeenStorage.get(promptLastSeenStorag
 
 const setPromptLastClosed = () => promptLastSeenStorage.set(promptLastSeenStorageKey, Date.now());
 
-// show if not logged in, not on a barrier page, the barrier has been seen in this session, and closed the prompt in the last 30 days
+const getTlsVersion = () => fetch('https://www.howsmyssl.com/a/check')
+	.then(response => response.json())
+	.then(({ tls_version = '' } = { }) => Number.parseFloat(tls_version.replace('TLS ', '')));
+
+/**
+ * Show the prompt if
+ *	* not logged in
+ *	* not on a barrier page
+ *	* the barrier has been seen in this session
+ *	* the prompt has not been closed, or was last closed more than 30 days ago
+ *	* browser's' TLS version is > 1.1
+ */
 const shouldPromptBeShown = () => {
 	if (isLoggedIn() || document.querySelector('.ft-subscription-panel')) {
 		return Promise.resolve(false);
 	} else {
-		return Promise.all([getProductSelectorLastSeen(), getPromptLastClosed()])
-			.then(([barrierLastSeen, promptLastClosed]) =>
-				barrierLastSeen && (!promptLastClosed || promptLastClosed + (1000 * 60 * 60 * 24 * 30) <= Date.now())
+		return Promise.all([getProductSelectorLastSeen(), getPromptLastClosed(), getTlsVersion()])
+			.then(([barrierLastSeen, promptLastClosed, tlsVersion]) =>
+				barrierLastSeen && (!promptLastClosed || promptLastClosed + (1000 * 60 * 60 * 24 * 30) <= Date.now()) && tlsVersion > 1.1
 			);
 	}
 };
@@ -126,4 +137,12 @@ export const init = () =>
 						return createSubscriptionPrompt(subscriptionValues);
 					});
 			}
+		})
+		.catch(error => {
+			broadcast('oErrors.log', {
+				error,
+				info: {
+					message: 'Error initialising subscription offer prompt'
+				}
+			})
 		});
