cef3d60fbef304d1937ee7359047854eea8c922c
Vlad Dubrovskis <vlad@vovka.co.uk>
2016-10-26 15:25:13 +0100

    fix a horrible type

--- ads/js/sticky.js
+++ ads/js/sticky.js
@@ -130,9 +130,7 @@ Sticky.prototype.timeoutHandler = function () {
 }
 
 Sticky.prototype.init = function () {
-	if (!this.fixed || !this.sibling || !this.boundary || window.pageYOffset > 0 || window.scrollY > 0 || window.navigator.userAgent.match(/i.*OS\s(\d+)_(\d+)/))
-
-) {
+	if (!this.fixed || !this.sibling || !this.boundary || window.pageYOffset > 0 || window.scrollY > 0 || window.navigator.userAgent.match(/i.*OS\s(\d+)_(\d+)/)){
 		return;
 	};
 	this.windowWidth = window.innerWidth;
