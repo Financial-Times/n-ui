1aeca6abfd0320add6f474dfbbb3b289436ad3e8
Vlad Dubrovskis <vlad@vovka.co.uk>
2016-10-26 16:12:37 +0100

    add a comment explaining why we exclude iOS

--- ads/js/sticky.js
+++ ads/js/sticky.js
@@ -130,6 +130,7 @@ Sticky.prototype.timeoutHandler = function () {
 }
 
 Sticky.prototype.init = function () {
+	// do not init if user already started scrolling or on iOS devices as the stickiness behaves flaky on iOS. Issue: ADS-1112
 	if (!this.fixed || !this.sibling || !this.boundary || window.pageYOffset > 0 || window.scrollY > 0 || window.navigator.userAgent.match(/i.*OS\s(\d+)_(\d+)/)){
 		return;
 	};
