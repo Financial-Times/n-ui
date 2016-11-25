738ca4d648331775a74fef4bf6d50306ac1d4597
Rhys Evans <rhys.evans@ft.com>
2016-11-25 09:54:31 +0000

    little more reacty style

--- header/js/suggestion-list.js
+++ header/js/suggestion-list.js
@@ -1,11 +1,14 @@
 const React = require('react');
 
 export class SuggestionList extends React.Component {
+	constructor () {
+		super();
+		this.state = {suggestions: []}
+	}
 	render () {
-		const suggestions = this.state.suggestions || [];
-		const hasSuggestions = !suggestions.length;
+		const hasSuggestions = !this.state.suggestions.length;
 		return <ul className="o-header__typeahead" hidden={hasSuggestions}>
-	{ suggestions.map(suggestion => (
+	{ this.state.suggestions.map(suggestion => (
 			<li className="o-header__typeahead-item">
 				<a className="o-header__typeahead-link" href={suggestion.url} data-trackable="typeahead" data-concept-id={suggestion.id} dangerouslySetInnerHTML={{__html:suggestion.name}} ></a>
 			</li>
