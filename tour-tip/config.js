f28634234a77ed3feaf63840428d3a91564fdcc2
Rhys Evans <rhys.evans@ft.com>
2016-11-25 14:30:59 +0000

    fetch assets from __assets/creatives

--- tour-tip/config.js
+++ tour-tip/config.js
@@ -11,7 +11,7 @@ module.exports = {
 			content: {
 				title: 'Track the topics most important to you all in one place',
 				imageAlt: 'Illustration of the "Companies" page, showing an "Added" button next to the page heading',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/myft-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/myft-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -28,7 +28,7 @@ module.exports = {
 			content: {
 				title: 'Track the topics most important to you all in one place',
 				imageAlt: 'Illustration of the "Companies" page, showing an "Added" button next to the page heading',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/myft-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/myft-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -47,7 +47,7 @@ module.exports = {
 				title: 'Make myFT yours',
 				body: 'Discover thousands of topics to track. Then you can stay updated on those most important to you, all in one place.',
 				imageAlt: 'Illustration of the "Companies" page, showing an "Added" button next to the page heading',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/myft-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/myft-7.svg',
 				ctas: [{
 					url: '/tour/myft',
 					label: 'Explore myFT'
@@ -64,7 +64,7 @@ module.exports = {
 			content: {
 				title: 'Share 10 articles a month with non-subscribers, who can read them – absolutely free',
 				imageAlt: 'Illustration of the Gift Article button in article pages',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/gift-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/gift-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -81,7 +81,7 @@ module.exports = {
 			content: {
 				title: 'Share 10 articles a month with non-subscribers, who can read them – absolutely free',
 				imageAlt: 'Illustration of the Gift Article button in article pages',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/gift-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/gift-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -100,7 +100,7 @@ module.exports = {
 				title: 'Gift an FT article',
 				body: 'Start the conversation. Share up to 10 articles a month with colleagues, friends or family – absolutely free. Even if they’re not subscribed to the FT.',
 				imageAlt: 'Illustration of the Gift Article button in article pages',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/gift-7.svg'
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/gift-7.svg'
 			}
 		},
 		{
@@ -113,7 +113,7 @@ module.exports = {
 			content: {
 				title: 'Better filtering of trending topics in fastFT’s market-moving news',
 				imageAlt: 'Illustration of the fastFT page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/fastft-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/fastft-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -130,7 +130,7 @@ module.exports = {
 			content: {
 				title: 'Better filtering of trending topics in fastFT’s market-moving news',
 				imageAlt: 'Illustration of the fastFT page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/fastft-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/fastft-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -149,7 +149,7 @@ module.exports = {
 				title: 'Improved fastFT',
 				body: 'Better filtering of trending topics, same instant market-moving news and views from our global team.',
 				imageAlt: 'Illustration of the fastFT page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/fastft-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/fastft-7.svg',
 				ctas: [{
 					url: 'https://www.ft.com/fastft',
 					label: 'Try fastFT'
@@ -166,7 +166,7 @@ module.exports = {
 			content: {
 				title: 'Get intuitive markets data at a glance on any device',
 				imageAlt: 'Illustration of a graph in a Markets Data page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/markets-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/markets-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -183,7 +183,7 @@ module.exports = {
 			content: {
 				title: 'Get intuitive markets data at a glance on any device',
 				imageAlt: 'Illustration of a graph in a Markets Data page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/markets-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/markets-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -202,7 +202,7 @@ module.exports = {
 				title: 'Improved Markets Data tools',
 				body: 'Manage price alerts on the go, compare cross currencies quickly and get a macromap view of major indices across the globe.',
 				imageAlt: 'Illustration of a graph in a Markets Data page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/markets-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/markets-7.svg',
 				ctas: [{
 					url: 'http://markets.ft.com/data/',
 					label: 'Visit Markets Data'
@@ -219,7 +219,7 @@ module.exports = {
 			content: {
 				title: 'Instant, Daily or Weekly news digests delivered straight to your email inbox',
 				imageAlt: 'Illustration of a myFT Instant Alert email',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/alerts-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/alerts-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -236,7 +236,7 @@ module.exports = {
 			content: {
 				title: 'Instant, Daily or Weekly news digests delivered straight to your email inbox',
 				imageAlt: 'Illustration of a myFT Instant Alert email',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/alerts-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/alerts-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -255,7 +255,7 @@ module.exports = {
 				title: 'myFT Alerts',
 				body: 'Choose to receive Instant, Daily or Weekly digests about the topics in your myFT – delivered straight to your email inbox.',
 				imageAlt: 'Illustration of a myFT Instant Alert email',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/alerts-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/alerts-7.svg',
 				ctas: [{
 					url: '/myft/alerts',
 					label: 'Manage alerts'
@@ -272,7 +272,7 @@ module.exports = {
 			content: {
 				title: 'Email briefings subscriptions can now be handled in myFT',
 				imageAlt: 'Illustration of a News briefing preference in the myFT Alerts page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/briefings-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/briefings-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -289,7 +289,7 @@ module.exports = {
 			content: {
 				title: 'Email briefings subscriptions can now be handled in myFT',
 				imageAlt: 'Illustration of a News briefing preference in the myFT Alerts page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/briefings-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/briefings-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -308,7 +308,7 @@ module.exports = {
 				title: 'FT Email Briefings',
 				body: 'You can now manage your news briefing subscriptions in myFT. Keep an eye out for further enhancements coming soon.',
 				imageAlt: 'Illustration of a News briefing preference in the myFT Alerts page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/briefings-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/briefings-7.svg',
 				ctas: [{
 					url: 'https://www.ft.com/myft/alerts',
 					label: 'Review briefings'
@@ -325,7 +325,7 @@ module.exports = {
 			content: {
 				title: 'You can now add, edit and track investments on all your devices',
 				imageAlt: 'Illustration of the FT Portfolio page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/portfolio-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/portfolio-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -342,7 +342,7 @@ module.exports = {
 			content: {
 				title: 'You can now add, edit and track investments on all your devices',
 				imageAlt: 'Illustration of the FT Portfolio page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/portfolio-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/portfolio-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -361,7 +361,7 @@ module.exports = {
 				title: 'FT Portfolio tool',
 				body: 'Add, edit and track investments on all your devices. If you’re not quite ready to make the move, you can still access the old portfolio tool.',
 				imageAlt: 'Illustration of the FT Portfolio page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/portfolio-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/portfolio-7.svg',
 				ctas: [{
 					url: 'http://markets.ft.com/data/portfolio/dashboard',
 					label: 'Check your Portfolio'
@@ -378,7 +378,7 @@ module.exports = {
 			content: {
 				title: 'Valuable reports on specific countries, industries and business topics',
 				imageAlt: 'Illustration of a Special Reports page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/reports-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/reports-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -395,7 +395,7 @@ module.exports = {
 			content: {
 				title: 'Valuable reports on specific countries, industries and business topics',
 				imageAlt: 'Illustration of a Special Reports page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/reports-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/reports-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -414,7 +414,7 @@ module.exports = {
 				title: 'FT Special Reports',
 				body: 'Get valuable insights into specific countries, industries and business topics, with hundreds of Special Reports published every year.',
 				imageAlt: 'Illustration of a Special Reports page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/reports-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/reports-7.svg',
 				ctas: [{
 					url: 'https://www.ft.com/special-reports',
 					label: 'View Special Reports'
@@ -431,7 +431,7 @@ module.exports = {
 			content: {
 				title: 'Read live updates of the latest FT news, the moment the stories are published',
 				imageAlt: 'Illustration of the FT News feed page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/feed-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/feed-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'More tips'
@@ -448,7 +448,7 @@ module.exports = {
 			content: {
 				title: 'Read live updates of the latest FT news, the moment the stories are published',
 				imageAlt: 'Illustration of the FT News feed page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/feed-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/feed-7.svg',
 				ctas: [{
 					url: '/tour',
 					label: 'Discover more tips'
@@ -467,7 +467,7 @@ module.exports = {
 				title: 'FT News Feed',
 				body: 'The news as it’s happening. Read live updates of the latest FT news, the moment the stories are published.',
 				imageAlt: 'Illustration of the FT News feed page',
-				imageUrl: 'https://next-geebee.ft.com/assets/tour/feed-7.svg',
+				imageUrl: 'https://www.ft.com/__assets/creatives/tour/feed-7.svg',
 				ctas: [{
 					url: 'https://www.ft.com/news-feed',
 					label: 'Visit News Feed'
