28d29a55ed6aa4975c10855d59086f5595d02be3
Laurie Boyes <laurie.boyes@ft.com>
2016-12-07 16:16:32 +0000

    both results cols always displayed, sorry nothing messages

--- header/js/suggestion-list.js
+++ header/js/suggestion-list.js
@@ -19,7 +19,7 @@ export class SuggestionList extends React.Component {
 		return text.replace(RegExp(regExpEscape(this.state.searchTerm), 'gi'), '<mark>$&</mark>');
 	}
 
-	tailLink (group) {
+	renderTailLink (group) {
 		if (group.tailLink) {
 			const linkAttrs = {
 				className: 'o-header__typeahead-link o-header__typeahead-link--tail',
@@ -31,62 +31,74 @@ export class SuggestionList extends React.Component {
 		}
 	}
 
+
+	renderItems(group) {
+
+		if(!group.suggestions.length && group.emptyHtml) {
+			return group.emptyHtml;
+		} else {
+			return <ul className="o-header__typeahead-item-list">
+				{ group.suggestions.map(suggestion => (
+					<li className="o-header__typeahead-item">
+						<a className={'o-header__typeahead-link ' + group.linkClassName}
+						   href={suggestion.url}
+						   data-trackable="link"
+						   data-concept-id={suggestion.id}
+						   dangerouslySetInnerHTML={{__html:suggestion.html}} ></a>
+					</li>
+				)) }
+				<li className="o-header__typeahead-item">
+					{this.renderTailLink(group)}
+				</li>
+			</ul>
+		}
+	}
+
 	render () {
 		const hasTags = this.state.suggestions.tags.length;
 		const hasEquities = this.state.suggestions.equities.length;
 		const hasSuggestions = hasTags || hasEquities;
-		const suggestions = [];
-		if (hasTags) {
-			suggestions.push({
+		const suggestions = [
+			{
 				heading: 'News',
 				linkClassName: 'o-header__typeahead-link--news',
 				trackable: 'news',
 				suggestions: this.state.suggestions.tags.slice(0,6)
 					.map(suggestion => ({
-							html: this.highlight(suggestion.name),
-							url: suggestion.url,
-							id: suggestion.id
-					}))
-			})
-		}
-
-		if (hasEquities) {
-			suggestions.push({
+						html: this.highlight(suggestion.name),
+						url: suggestion.url,
+						id: suggestion.id
+					})),
+				tailLink: {
+					url: `/search?q=${this.state.searchTerm}`, // todo whoops xss
+					text: `Search for all content matching ${this.state.searchTerm}`, // todo whoops xss
+					trackable: 'see-all'
+				}
+			},
+			{
 				heading: 'Equities',
 				trackable: 'equities',
 				linkClassName: 'o-header__typeahead-link--equities',
+				emptyHtml: (<span>lol no</span>),
 				suggestions: this.state.suggestions.equities.slice(0,6)
 					.map(suggestion => ({
-							html: `<span class="o-header__typeahead-link__equity-name">${this.highlight(suggestion.name)}</span><abbr>${this.highlight(suggestion.symbol)}</abbr>`,
-							url: suggestion.url,
-							id: suggestion.symbol
+						html: `<span class="o-header__typeahead-link__equity-name">${this.highlight(suggestion.name)}</span><abbr>${this.highlight(suggestion.symbol)}</abbr>`,
+						url: suggestion.url,
+						id: suggestion.symbol
 					})),
 				tailLink: {
 					url: `https://markets.ft.com/data/search?query=${this.state.searchTerm}`, // todo whoops xss
-					text: 'See all quotes matching app',
+					text: `See all quotes matching ${this.state.searchTerm}`, // todo whoops xss
 					trackable: 'see-all'
 				}
-			})
-		}
+			}
+		];
 
 		return <div className="o-header__typeahead" hidden={ !hasSuggestions} data-trackable="typeahead">
 		{ suggestions.map(group => (
 				<div className={'o-header__typeahead__group ' + group.className} data-trackable={group.trackable}>
 					<h3 className="o-header__typeahead-heading">{group.heading}</h3>
-					<ul className="o-header__typeahead-item-list">
-					{ group.suggestions.map(suggestion => (
-						<li className="o-header__typeahead-item">
-							<a className={'o-header__typeahead-link ' + group.linkClassName}
-								href={suggestion.url}
-								data-trackable="link"
-								data-concept-id={suggestion.id}
-								dangerouslySetInnerHTML={{__html:suggestion.html}} ></a>
-						</li>
-					)) }
-						<li className="o-header__typeahead-item">
-							{this.tailLink(group)}
-						</li>
-					</ul>
+					{this.renderItems(group)}
 				</div>
 			)) }
 		</div>
