176859a24823ae211c02182680e85d4c7833bbe4
Laurie Boyes <laurie.boyes@ft.com>
2016-12-07 12:40:49 +0000

    see all quotes link and more granular tracking

--- header/js/suggestion-list.js
+++ header/js/suggestion-list.js
@@ -2,7 +2,7 @@ const React = require('react');
 
 function regExpEscape (s) {
 	return s.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
-};
+}
 
 export class SuggestionList extends React.Component {
 	constructor () {
@@ -19,6 +19,18 @@ export class SuggestionList extends React.Component {
 		return text.replace(RegExp(regExpEscape(this.state.searchTerm), 'gi'), '<mark>$&</mark>');
 	}
 
+	tailLink (group) {
+		if (group.tailLink) {
+			const linkAttrs = {
+				className: 'o-header__typeahead-link o-header__typeahead-link--tail',
+				href: group.tailLink.url,
+				'data-trackable': group.tailLink.trackable
+			};
+
+			return <a {...linkAttrs} >{group.tailLink.text}</a>
+		}
+	}
+
 	render () {
 		const hasTags = this.state.suggestions.tags.length;
 		const hasEquities = this.state.suggestions.equities.length;
@@ -27,6 +39,7 @@ export class SuggestionList extends React.Component {
 		if (hasTags) {
 			suggestions.push({
 				heading: 'News',
+				trackable: 'news',
 				suggestions: this.state.suggestions.tags.slice(0,6)
 					.map(suggestion => ({
 							html: this.highlight(suggestion.name),
@@ -39,32 +52,41 @@ export class SuggestionList extends React.Component {
 		if (hasEquities) {
 			suggestions.push({
 				heading: 'Equities',
+				trackable: 'equities',
 				suggestions: this.state.suggestions.equities.slice(0,6)
 					.map(suggestion => ({
 							html: `<span class="o-header__typeahead-link__equity-name">${this.highlight(suggestion.name)}</span><abbr>${this.highlight(suggestion.symbol)}</abbr>`,
 							url: suggestion.url,
 							id: suggestion.symbol
-					}))
+					})),
+				tailLink: {
+					url: `https://markets.ft.com/data/search?query=${this.state.searchTerm}`, // todo whoops xss
+					text: 'See all quotes matching app',
+					trackable: 'see-all'
+				}
 			})
 		}
 
-		return <div className="o-header__typeahead" hidden={ !hasSuggestions}>
+		return <div className="o-header__typeahead" hidden={ !hasSuggestions} data-trackable="typeahead">
 		{ suggestions.map(group => (
-				<div className="o-header__typeahead__group">
+				<div className="o-header__typeahead__group" data-trackable={group.trackable}>
 					<h3 className="o-header__typeahead-heading">{group.heading}</h3>
 					<ul className="o-header__typeahead-item-list">
 					{ group.suggestions.map(suggestion => (
 						<li className="o-header__typeahead-item">
 							<a className="o-header__typeahead-link"
 								href={suggestion.url}
-								data-trackable="typeahead"
+								data-trackable="link"
 								data-concept-id={suggestion.id}
 								dangerouslySetInnerHTML={{__html:suggestion.html}} ></a>
 						</li>
 					)) }
+						<li className="o-header__typeahead-item">
+							{this.tailLink(group)}
+						</li>
 					</ul>
 				</div>
 			)) }
 		</div>
 	}
-};
+}
