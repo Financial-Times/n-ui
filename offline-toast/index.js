824b70ce6c2fe9a292dd04a08cbbe22c7d6a6027
Simon Legg <leggsimon@gmail.com>
2016-12-15 12:32:57 +0000

    use service worker message event

--- offline-toast/index.js
+++ offline-toast/index.js
@@ -1,16 +1,24 @@
 const toast = document.getElementById('offline-notification-toast')
 
-function show () {
-  toast.classList.add('display')
-  setTimeout(hide, 5000)
-}
-
-function hide () {
-  toast.classList.remove('display')
+function show (msg) {
+	document.getElementById('offline-notification-toast__message').textContent = msg
+	toast.classList.add('display')
+	setTimeout(function () {
+		toast.classList.remove('display')
+	}, 5000)
 }
 
 function init () {
-  document.addEventListener('cacheUpdated', show)
+	navigator.serviceWorker.addEventListener('message', function (event) {
+		const data = event.data;
+		const command = data.command;
+		if (command === 'precacheDone' && data.data.message === '') {
+			show('Read FT top stories even when you\'re offline')
+		}
+		if (command === 'cacheUpdated') {
+			show('Cache has been updated')
+		}
+	})
 }
 
 module.exports = { init }
