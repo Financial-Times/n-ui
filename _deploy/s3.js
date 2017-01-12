14cf035c70277264dea776fdc0b41cbf797fe0a1
Rhys Evans <rhys.evans@ft.com>
2017-01-12 14:40:31 +0000

    brotlifying assets, and better metric collection (stabler names)

--- _deploy/s3.js
+++ _deploy/s3.js
@@ -3,6 +3,13 @@ const shellpromise = require('shellpromise');
 const semver = require('semver');
 const fetch = require('node-fetch');
 const deployStatic = require('@financial-times/n-heroku-tools').deployStatic.task;
+const brotli = require('brotli');
+const denodeify = require('denodeify');
+const path = require('path')
+const fs = require('fs');
+const readFile = denodeify(fs.readFile);
+const writeFile = denodeify(fs.writeFile);
+
 let tag = process.env.CIRCLE_TAG;
 let versions;
 let isOfficialRelease = false;
@@ -46,33 +53,53 @@ function purge (path) {
 		.then(() => purgeOnce(path, '...gone!'))
 }
 
-shellpromise('find . -path "./dist/*"')
-	.then(files => {
-		files = files.split('\n')
-			.filter(f => !!f)
-			.filter(f => !/^n-ui-core\.css/.test(f));
+function brotlify () {
+	return shellpromise('find . -path "./dist/*"')
+		.then(files => Promise.all(
+			files.split('\n')
+				.filter(f => !!f && /\.(js|css)$/.test(f))
+				.map(f => f.trim())
+				.map(fileName =>
+					readFile(path.join(process.cwd(), fileName), 'utf8')
+						.then(brotli.compress)
+						.then(contents => writeFile(path.join(process.cwd(), fileName + '.brotli'), contents))
+				)
+		))
+}
 
-		return Promise.all(
-			versions
-				.map((version, i) => {
-					return deployStatic({
-						files: files,
-						destination: `n-ui/cached/${version}`,
-						bucket: 'ft-next-n-ui-prod',
-						strip: 1,
-						monitor: isOfficialRelease && i === 0, // only monitor the size of the first copy deployed
-						cacheControl: 'must-revalidate, max-age=1200',
-						surrogateControl: 'must-revalidate, max-age=3600, stale-while-revalidate=60, stale-on-error=86400'
-					})
-						.then(() => files.map(file => {
-							const paths = [
-								`https://www.ft.com/__assets/n-ui/cached/${version}/${file.split('/').pop()}`
-							];
-							return Promise.all(paths.map(purge));
-						}))
-				})
+
+function getDeployList () {
+	return shellpromise('find . -path "./dist/*"')
+		.then(files =>
+			files.split('\n')
+				.filter(f => !!f)
+				.filter(f => !/^n-ui-core\.css/.test(f))
 		)
-	})
+}
+
+brotlify()
+	.then(getDeployList)
+	.then(files => Promise.all(
+		versions
+			.map((version, i) => {
+				return deployStatic({
+					files: files,
+					destination: `n-ui/cached/${version}`,
+					bucket: 'ft-next-n-ui-prod',
+					strip: 1,
+					monitor: isOfficialRelease && i === 0, // only monitor the size of the first copy deployed,
+					monitorStripDirectories: true,
+					cacheControl: 'must-revalidate, max-age=1200',
+					surrogateControl: 'must-revalidate, max-age=3600, stale-while-revalidate=60, stale-on-error=86400'
+				})
+					.then(() => files.map(file => {
+						const paths = [
+							`https://www.ft.com/__assets/n-ui/cached/${version}/${file.split('/').pop()}`
+						];
+						return Promise.all(paths.map(purge));
+					}))
+			})
+	))
 	.catch(err => {
 		console.log(err) //eslint-disable-line
 		process.exit(2)
