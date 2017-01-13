c67689de9f60a6ccfbfb7382793f1903f111be9c
Rhys Evans <rhys.evans@ft.com>
2017-01-13 14:08:43 +0000

    baby steps towards polling to update layout templates

--- node/src/handlebars/index.js
+++ node/src/handlebars/index.js
@@ -1,4 +1,9 @@
 const handlebars = require('@financial-times/n-handlebars');
+const Poller = require('ft-poller');
+const fs = require('fs');
+const vm = require('vm');
+// todo calculate properly
+const majorVersion = 'v2'
 
 module.exports = function (conf) {
 	const app = conf.app;
@@ -16,6 +21,11 @@ module.exports = function (conf) {
 		};
 	}
 
+	// always enable in-memory view caching
+	// - needed in prod to allow polling for layout updates
+	// - in dev most changes result in the app restarting anyway, so in memory caching shouldn't impair development
+	app.enable('view cache');
+
 	if (options.partialsDirectory) {
 		partialsDir.push(options.partialsDirectory);
 	}
@@ -28,4 +38,57 @@ module.exports = function (conf) {
 		directory: directory,
 		viewsDirectory: options.viewsDirectory
 	})
+		.then(instance => {
+			if (process.env.NEXT_APP_SHELL !== 'local' && process.env.TRIAL_POLLING_LAYOUTS === 'true') {
+				return shellpromise('ls ./layout/*.html')
+					.then(files =>
+						files.split('\n')
+							.filter(f => !!f)
+							.map(f => `https://ft-next-n-ui-prod.s3-eu-west-1.amazonaws.com/templates/${majorVersion}/${f.replace(/^\.\//, '')}.precompiled`)
+					)
+					// .then(urls => urls.concat(`https://ft-next-n-ui-prod.s3-eu-west-1.amazonaws.com/templates/${majorVersion}/latest.json`))
+					.then(urls => {
+						new Poller({
+							url: urls[1],
+							refreshInterval: 60000,
+							parseData: templates => {
+								templates
+									.map(tpl => {
+										tpl = tpl
+											.replace('</body>', `<div style=\\"background: white;position: absolute;top: 0;left:0;color:red;font-size:50px\\">${Date.now()}</div></body>`)
+										const script = new vm.Script(`(${tpl})`);
+									  const tplAsObj = script.runInNewContext();
+										instance.compiled[layoutsDir + '/wrapper.html'] = instance.handlebars.template(tplAsObj);
+									})
+							},
+							autostart: true
+						})
+					})
+
+			}
+			return instance;
+		});
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
