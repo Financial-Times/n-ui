f5ced0d924b2e53c05b5c325061d94237d6ef84c
Rhys Evans <wheresrhys@gmail.com>
2017-01-19 11:54:14 +0000

    decomission old typeahead (#727)

--- header/index.js
+++ header/index.js
@@ -1,7 +1,6 @@
 const OHeader = require('o-header');
-const Typeahead = require('./js/typeahead');
 const promoHandler = require('./js/promoHandler');
-const TypeaheadNew = require('../typeahead');
+const Typeahead = require('../typeahead');
 
 function init (flags) {
 	promoHandler.init(flags);
@@ -13,19 +12,10 @@ function init (flags) {
 
 	const typeaheadElements = document.querySelectorAll('.o-header [data-typeahead], .o-header__drawer [data-typeahead]');
 
-	if (flags.get('typeahead') && typeaheadElements.length) {
-		if (flags.get('searchMultiTypeahead')) {
-			for (let i = 0; i < typeaheadElements.length; i++) {
-				const form = typeaheadElements[i];
-				new TypeaheadNew(form);
-			}
-
-		} else {
-
-			for (let i = 0; i < typeaheadElements.length; i++) {
-				let element = typeaheadElements[i]
-				new Typeahead( element, `//${window.location.host}/search-suggestions?q=` );
-			}
+	if (flags.get('searchMultiTypeahead') && typeaheadElements.length) {
+		for (let i = 0; i < typeaheadElements.length; i++) {
+			const form = typeaheadElements[i];
+			new Typeahead(form);
 		}
 	}
 }
