9d32d03b4c0b2b0c0cddb5f4f320a07c08091372
Rhys Evans <rhys.evans@ft.com>
2017-02-06 14:12:55 +0000

    added tests for user sampling

--- utils/index.js
+++ utils/index.js
@@ -1,14 +1,9 @@
 const cookieStore = require('./js/cookies');
 
-let spoorNumber;
-
 const getSpoorNumber = () => {
-	if (!spoorNumber) {
-		let spoorId = cookieStore.get('spoor-id').replace(/-/g, '');
-		spoorId = spoorId.substring(spoorId.length - 12, spoorId.length); // Don't overflow the int
-		spoorNumber = parseInt(spoorId, 16);
-	}
-	return spoorNumber;
+	let spoorId = cookieStore.get('spoor-id').replace(/-/g, '');
+	spoorId = spoorId.substring(spoorId.length - 12, spoorId.length); // Don't overflow the int
+	return parseInt(spoorId, 16);
 }
 
 module.exports = {
@@ -76,9 +71,9 @@ module.exports = {
 		if (!seed) {
 			throw new Error('sampleUsers needs a seed string to be passed in as the second parameter')
 		}
-		const seedAsNumber = seed.split('').reduce((num, str) => num + str.charCodeAt(0));
+		const seedAsNumber = seed.split('').reduce((num, str, i) => num + Math.pow(2, i) * str.charCodeAt(0), 0);
 		return (getSpoorNumber() + seedAsNumber) % 100 < pct
-	}
+	},
 	cookieStore,
 
 	// legacy method - keeping for backwards compatibility
