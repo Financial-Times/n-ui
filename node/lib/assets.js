6098166943fa425ae46aa99461d45eaa6251b53c
Rhys Evans <wheresrhys@gmail.com>
2017-02-13 13:41:05 +0000

    Rhys/fix local n ui (#799)
    * turning on tests that weren't being run
    
    * slight refactor of assets loader
    
    * finished fixing n-ui local urls
    
    * Update assets.js

--- node/lib/assets.js
+++ node/lib/assets.js
@@ -5,8 +5,15 @@ const nUiManager = require('./n-ui-manager');
 const linkHeaderFactory = require('./link-header');
 const stylesheetManager = require('./stylesheet-manager');
 const messages = require('./messages');
+const hashedAssets = require('./hashed-assets');
+const verifyAssetsExist = require('./verify-assets-exist');
 
-module.exports = function (options, directory, hashedAssets) {
+function init (options, directory, locals) {
+
+	verifyAssetsExist.verify(locals);
+	const hasher = hashedAssets.init(locals);
+	nUiManager.init(directory, hasher);
+
 
 	const useLocalAppShell = process.env.NEXT_APP_SHELL === 'local';
 
@@ -21,76 +28,83 @@ module.exports = function (options, directory, hashedAssets) {
 
 
 	const stylesheets = stylesheetManager.getStylesheets(options, directory);
-	const linkHeader = linkHeaderFactory(hashedAssets);
-	const nUiUrlRoot = nUiManager.getUrlRoot(hashedAssets);
+	const linkHeader = linkHeaderFactory(hasher);
+	const nUiUrlRoot = nUiManager.getUrlRoot(hasher);
 
-	return (req, res, next) => {
+	return {
+		hasher,
+		middleware: (req, res, next) => {
 
-		// This middleware relies on the presence of res.locals.flags.
-		// In some scenarios (e.g. using handlebars but not flags) this
-		// won't be present
-		const flags = res.locals.flags || {};
+			// This middleware relies on the presence of res.locals.flags.
+			// In some scenarios (e.g. using handlebars but not flags) this
+			// won't be present
+			const flags = res.locals.flags || {};
 
-		// define a helper for adding a link header
-		res.linkResource = linkHeader;
+			// define a helper for adding a link header
+			res.linkResource = linkHeader;
 
-		res.locals.stylesheets = stylesheets;
+			res.locals.stylesheets = stylesheets;
 
-		if (req.accepts('text/html')) {
-			res.locals.javascriptBundles = [];
-			res.locals.cssBundles = [];
-			res.locals.criticalCss = [];
-			res.locals.nUiConfig = nUiConfig;
+			if (req.accepts('text/html')) {
+				res.locals.javascriptBundles = [];
+				res.locals.cssBundles = [];
+				res.locals.criticalCss = [];
+				res.locals.nUiConfig = nUiConfig;
 
-			// work out which assets will be required by the page
-			const polyfillRoot = `//${flags.polyfillQA ? 'qa.polyfill.io' : 'next-geebee.ft.com/polyfill'}/v2/polyfill.min.js`;
+				// work out which assets will be required by the page
+				const polyfillRoot = `//${flags.polyfillQA ? 'qa.polyfill.io' : 'next-geebee.ft.com/polyfill'}/v2/polyfill.min.js`;
 
-			res.locals.polyfillCallbackName = nPolyfillIo.callbackName;
-			res.locals.polyfillUrls = {
-				enhanced: polyfillRoot + nPolyfillIo.getQueryString({enhanced: true}),
-				core: polyfillRoot + nPolyfillIo.getQueryString({enhanced: false})
-			}
+				res.locals.polyfillCallbackName = nPolyfillIo.callbackName;
+				res.locals.polyfillUrls = {
+					enhanced: polyfillRoot + nPolyfillIo.getQueryString({enhanced: true}),
+					core: polyfillRoot + nPolyfillIo.getQueryString({enhanced: false})
+				}
 
-			res.locals.javascriptBundles.push(
-				`${nUiUrlRoot}es5${(flags.nUiBundleUnminified || useLocalAppShell ) ? '' : '.min'}.js`,
-				hashedAssets.get('main-without-n-ui.js'),
-				res.locals.polyfillUrls.enhanced
-			);
+				res.locals.javascriptBundles.push(
+					`${nUiUrlRoot}es5${(flags.nUiBundleUnminified || useLocalAppShell ) ? '' : '.min'}.js`,
+					hasher.get('main-without-n-ui.js'),
+					res.locals.polyfillUrls.enhanced
+				);
 
-			// output the default link headers just before rendering
-			const originalRender = res.render;
+				// output the default link headers just before rendering
+				const originalRender = res.render;
 
-			res.render = function (template, templateData) {
+				res.render = function (template, templateData) {
 
-				let cssVariant = templateData.cssVariant || res.locals.cssVariant;
-				cssVariant = cssVariant ? `-${cssVariant}` : '';
+					let cssVariant = templateData.cssVariant || res.locals.cssVariant;
+					cssVariant = cssVariant ? `-${cssVariant}` : '';
 
-				// define which css to output in the critical path
-				if (options.withHeadCss) {
-					if (`head${cssVariant}-n-ui-core` in stylesheets) {
-						res.locals.criticalCss.push(stylesheets[`head${cssVariant}-n-ui-core`])
+					// define which css to output in the critical path
+					if (options.withHeadCss) {
+						if (`head${cssVariant}-n-ui-core` in stylesheets) {
+							res.locals.criticalCss.push(stylesheets[`head${cssVariant}-n-ui-core`])
+						}
+						res.locals.criticalCss.push(stylesheets[`head${cssVariant}`]);
 					}
-					res.locals.criticalCss.push(stylesheets[`head${cssVariant}`]);
+
+					res.locals.cssBundles.push({
+						path: hasher.get(`main${cssVariant}.css`),
+						isMain: true,
+						isLazy: options.withHeadCss
+					});
+
+					res.locals.cssBundles.forEach(file => res.linkResource(file.path, {as: 'style'}));
+					res.locals.javascriptBundles.forEach(file => res.linkResource(file, {as: 'script'}));
+
+					if (templateData.withAssetPrecache) {
+						res.locals.cssBundles.forEach(file => res.linkResource(file.path, {as: 'style', rel: 'precache'}));
+						res.locals.javascriptBundles.forEach(file => res.linkResource(file, {as: 'script', rel: 'precache'}));
+					}
+
+					return originalRender.apply(res, [].slice.call(arguments));
 				}
-
-				res.locals.cssBundles.push({
-					path: hashedAssets.get(`main${cssVariant}.css`),
-					isMain: true,
-					isLazy: options.withHeadCss
-				});
-
-				res.locals.cssBundles.forEach(file => res.linkResource(file.path, {as: 'style'}));
-				res.locals.javascriptBundles.forEach(file => res.linkResource(file, {as: 'script'}));
-
-				if (templateData.withAssetPrecache) {
-					res.locals.cssBundles.forEach(file => res.linkResource(file.path, {as: 'style', rel: 'precache'}));
-					res.locals.javascriptBundles.forEach(file => res.linkResource(file, {as: 'script', rel: 'precache'}));
-				}
-
-				return originalRender.apply(res, [].slice.call(arguments));
 			}
-		}
 
-		next();
+			next();
+		}
 	}
+}
+
+module.exports = {
+	init
 }
