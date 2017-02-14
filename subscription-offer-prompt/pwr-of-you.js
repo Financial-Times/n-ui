785deaa534ffd6e5b45b75ce24175f52b579a3cb
Jennifer Kerr <jkerr321@gmail.com>
2017-02-14 12:19:00 +0000

    added test - some failing

--- subscription-offer-prompt/pwr-of-you.js
+++ subscription-offer-prompt/pwr-of-you.js
@@ -5,7 +5,7 @@ import * as utils from './utils';
 import { broadcast } from '../utils';
 
 const promptLastSeenStorage = new Superstore('local', 'n-ui.subscription-offer-pwr-of-you');
-const promptLastSeenStorageKey = 'last-closed';
+const promptLastSeenStorageKey = 'last-closed-pwr';
 
 const getPromptLastClosed = () => promptLastSeenStorage.get(promptLastSeenStorageKey);
 
@@ -17,9 +17,13 @@ const setPromptLastClosed = () => promptLastSeenStorage.set(promptLastSeenStorag
  *	* usElection2016DiscountSlider flag is true
  */
 const shouldPromptBeShown = (flags) => {
-	// console.log("here shouldPromptBeShown")
+	console.log("here shouldPromptBeShown")
 	return getPromptLastClosed()
 			.then(promptLastClosed => {
+				console.log("here getPromptLastClosed")
+				console.log('FLAGS', flags)
+				console.log("flag = ", flags.get('PowerOfYouSlider'));
+				console.log("promptLastClosed = ", promptLastClosed);
 				return (!promptLastClosed || promptLastClosed + (1000 * 60 * 60 * 24 * 30) <= Date.now()) && flags.get('PowerOfYouSlider');
 			})
 };
@@ -34,7 +38,6 @@ const popupTemplate = () => `
 		<div class="o-grid-row">
 			<div data-o-grid-colspan="12">
 				<img src="https://www.ft.com/__origami/service/image/v2/images/raw/https%3A%2F%2Fwww.ft.com%2F__assets%2Fcreatives%2Fthird-party%2FPowr-of-You-Logo_3600w.png?width=250&amp;source=next&amp;fit=scale-down" alt="Power of you logo"role="presentation" aria-hidden="true">
-				<!-- <img src="{{#resize 300}}//www.ft.com/__assets/creatives/third-party/Powr-of-You-Logo_3600w.png{{/resize}}"> -->
 			</div>
 		</div>
 		<div class="o-grid-row subscription-prompt--pwr__lead">
@@ -50,10 +53,10 @@ const popupTemplate = () => `
 		</div>
 		<div class="o-grid-row">
 			<div data-o-grid-colspan="6">
-				<a class="o-buttons o-buttons--big subscription-prompt--pwr__button" href="https://www.powrofyou.com/?utm_source=ext&utm_campaign=ft">Tell me more </a>
+				<a class="o-buttons o-buttons--big subscription-prompt--pwr__button" data-trackable="pwr_tell-me-more" href="https://www.powrofyou.com/?utm_source=ext&utm_campaign=ft">Tell me more </a>
 			</div>
 			<div data-o-grid-colspan="6">
-				<a class="o-buttons o-buttons--big o-buttons--standout subscription-prompt--pwr__button" href="https://www.powrofyou.com/JoinUs?utm_source=ext&utm_campaign=ft">Yes</a>
+				<a class="o-buttons o-buttons--big o-buttons--standout subscription-prompt--pwr__button" data-trackable="pwr_sign-up" href="https://www.powrofyou.com/JoinUs?utm_source=ext&utm_campaign=ft">Yes</a>
 			</div>
 		</div>
 	</aside>
@@ -67,12 +70,17 @@ const createPopupHTML = () =>
 	}, popupTemplate());
 
 const createPrompt = () => {
+	log("here createPrompt");
 	const prompt = createPopupHTML();
 	prompt.onClose = setPromptLastClosed;
 	document.body.appendChild(prompt);
 	const slidingPopup = new SlidingPopup(prompt);
+	console.log("slidingPopup = ", slidingPopup);
+	console.log("promt = ", prompt);
+
 	setTimeout(() => {
 		slidingPopup.open();
+
 
 		// TODO find out what tracking we need
 		broadcast('oTracking.event', {
@@ -87,17 +95,16 @@ const createPrompt = () => {
 };
 
 export const init = (flags) => {
-	// console.log("here 1");
 	return shouldPromptBeShown(flags)
 		.then(shouldShow => {
-			// console.log("here 3");
+			console.log("shouldShow = ", shouldShow);
 			if (shouldShow) {
 				return createPrompt();
-				// console.log("here 4");
 			}
 		})
 		.catch(error => {
-			// console.log(error);
+			console.log("reached error");
+			console.log(error);
 			broadcast('oErrors.log', {
 				error,
 				info: {
