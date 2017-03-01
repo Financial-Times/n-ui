37aee017f0418fc1cc76e595873390f584cac595
Rhys Evans <rhys.evans@ft.com>
2017-03-01 12:59:12 +0000

    basic build task working successfully

--- bin/nui.js
+++ bin/nui.js
@@ -6,6 +6,10 @@ const colors = require('colors');
 const util = require('util');
 const fs = require('fs');
 const path = require('path');
+const ratRace = require('promise-rat-race');
+const nEagerFetch = require('n-eager-fetch');
+const shellpromise = require('shellpromise');
+const nUiManager = require('../node/lib/n-ui-manager')
 
 function shell (processToRun, options) {
 	options = options || {};
@@ -72,36 +76,93 @@ const utils = {
 	}
 };
 
+const aboutJson = () => {
+	return shellpromise('git rev-parse HEAD | xargs echo -n')
+		.then(version => {
+			return {
+				description: require(path.join(process.cwd(), '/package.json')).name,
+				support: 'next.team@ft.com',
+				supportStatus: 'active',
+				appVersion: version,
+				buildCompletionTime: new Date().toISOString(),
+				nUiVersion: require('../package.json').version
+			}
+		})
+		.then(about => fs.writeFileSync(path.join(process.cwd(), '/public/__about.json'), JSON.stringify(about, null, 2)))
+}
+
+const devAdvice = () => {
+	if (!process.env.CIRCLE_BRANCH) {
+		logger.info('Developers: If you want your app to point to n-ui locally, then `export NEXT_APP_SHELL=local`')
+	}
+}
+
+
+
+
+const downloadHeadCss = () => {
+	if (process.env.LOCAL_APP_SHELL === 'local') {
+		return Promise.resolve();
+	}
+	return ratRace(
+		nUiManager.getReleaseRoots()
+			.map(urlRoot =>
+				nEagerFetch(`${urlRoot}head-n-ui-core.css`, {retry: 3})
+					.then(res => {
+						if (res.ok) {
+							return res.text();
+						}
+						throw new Error('Failed to fetch n-ui stylesheet');
+					})
+					.then(text => {
+						// if it's an empty string, something probably went wrong
+						if (!text.length) {
+							throw new Error('Fetched empty n-ui stylesheet');
+						}
+						return text;
+					})
+			)
+	)
+		.then(text => fs.writeFile(path.join(process.cwd(), 'public/head-n-ui-core.css'), text))
+		.then(() => logger.success('head-n-ui-core.css successfully retrieved from s3'))
+		.catch(err => {
+			logger.warn('failed to fetch head-n-ui-core.css from s3')
+			logger.warn(err)
+		})
+}
+const downloadAssets = () => {
+
+	return downloadHeadCss()
+		.then(() => {
+			if (!fs.existsSync(path.join(process.cwd(), 'public/head-n-ui-core.css'))) {
+				throw 'Missing head-n-ui-core.css file';
+			}
+		})
+		.catch(err => {
+			if (!process.env.CIRCLE_BRANCH) {
+				logger.info(`\
+If developing locally and you are having network problems, your app
+will be unable to download n-ui's assets. Try  \`export NEXT_APP_SHELL=local\`
+to force them to be built locally.
+`)
+			}
+			throw err;
+		})
+}
+
 program.version(require('../package.json').version);
 
 program
 	.command('build')
-	.description('Builds n-ui apps, ready to be deployed t oyour favourite s3 bucket or heroku host')
+	.description('Builds n-ui apps, ready to be deployed to your favourite s3 bucket or heroku host')
 	.option('--production', 'Builds with production settings')
-	.action(function (options) {
+	.action(options => {
 
-// # Remind developers that if they want to use a local version of n-ui,
-// # they need to `export NEXT_APP_SHELL=local`
-// dev-n-ui:
-// ifeq ($(NODE_ENV),) # Not production
-// ifeq ($(CIRCLE_BRANCH),) # Not CircleCI
-// ifneq ($(shell grep -s -Fim 1 n-ui bower.json),) # The app is using n-ui
-// ifneq ($(NEXT_APP_SHELL),local) # NEXT_APP_SHELL is not set to local
-// 	$(info Developers: If you want your app to point to n-ui locally, then `export NEXT_APP_SHELL=local`)
-// endif
-// endif
-// endif
-// endif
-//
-// make public/__about.json ... but need to apply to non Procfiles? else next-errors??
+		devAdvice();
 
 		shell(`webpack ${options.production ? '--bail' : '--dev'}`)
-			.then(() => {
-				//add to about json
-				//download n-ui files from network - force it, but fail gracefully if
-				//in dev and offline and files exist
-				//alert that appp-shell=local is a workaround for poor network
-			})
+			.then(aboutJson)
+			.then(downloadAssets)
 			.then(() => {
 				if (options.production && fs.existsSync(path.join(process.cwd(), 'Procfile'))) {
 					return shell('haikro build');
@@ -110,40 +171,15 @@ program
 			.catch(utils.exit)
 	});
 
-
-
-
 program
 	.command('watch')
-	.description('Builds n-ui apps, ready to be deployed t oyour favourite s3 bucket or heroku host')
-	.action(function (options) {
+	.description('Builds and watches front end of n-ui apps')
+	.action(() => {
 
-// # Remind developers that if they want to use a local version of n-ui,
-// # they need to `export NEXT_APP_SHELL=local`
-// dev-n-ui:
-// ifeq ($(NODE_ENV),) # Not production
-// ifeq ($(CIRCLE_BRANCH),) # Not CircleCI
-// ifneq ($(shell grep -s -Fim 1 n-ui bower.json),) # The app is using n-ui
-// ifneq ($(NEXT_APP_SHELL),local) # NEXT_APP_SHELL is not set to local
-// 	$(info Developers: If you want your app to point to n-ui locally, then `export NEXT_APP_SHELL=local`)
-// endif
-// endif
-// endif
-// endif
-//
-// make public/__about.json ... but need to apply to non Procfiles? else next-errors??
-//download n-ui files from network if not already present
-//.then
-		shell(`webpack --watch ${options.production ? '--bail' : '--dev'}`)
-			.then(() => {
+		devAdvice();
 
-				//how to force fetching them after reinstall of n-ui
-			})
-			.then(() => {
-				if (options.production && fs.existsSync(path.join(process.cwd(), 'Procfile'))) {
-					return shell('haikro build');
-				}
-			})
+		downloadAssets()
+			.then(() => shell('webpack --watch --dev')	)
 			.catch(utils.exit)
 	});
 
