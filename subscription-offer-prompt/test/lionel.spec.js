8fb227136715a6e8b9dd5251236b379bff8933cd
Laura Carvajal <laura.carvajal@gmail.com>
2017-03-06 19:49:24 +0000

    USA

--- subscription-offer-prompt/test/lionel.spec.js
+++ subscription-offer-prompt/test/lionel.spec.js
@@ -38,115 +38,115 @@ describe('Lionel Slider', function () {
 		]);
 	});
 
-	describe('"Lionel Slider" Subscription Offer Prompt', () => {
-
-		beforeEach(() => {
-			fetchStub
-				.withArgs('/country')
-				.returns(Promise.resolve({
-					json: () => Promise.resolve('GBR')
-				}));
-		});
-
-
-		it ('should be a popup', () =>
-			init(flags).then(popup => {
-				expect(popup).to.be.ok;
-			})
-		);
-
-
-		it('should show prompt if navigated from barrier page, not on a barrier page and hasnt been shown in 30 days', () =>
-			init(flags).then(popup => popup.should.be.an.instanceof(SlidingPopup))
-		);
-
-		it('should have correct attributes', () =>
-			init(flags).then(popup => {
-				popup.el.getAttribute('class').should.include('n-sliding-popup subscription-prompt');
-				popup.el.getAttribute('data-n-component').should.equal('o-sliding-popup');
-				popup.el.getAttribute('data-n-sliding-popup-position').should.equal('bottom left');
-			})
-		);
-
-		it('should have correct html when the priceFlashSale flag is on', () =>
-			init(flags).then(popup => {
-				popup.el.innerHTML.should.contain('Save 33% now')
-			})
-		);
-
-		it('should have correct html when the priceFlashSale flag is off', () => {
-				flags = { get: (val) => val === 'b2cMessagePrompt'};
-				init(flags).then(popup => {
-					popup.el.innerHTML.should.contain('You qualify for a 25% subscription discount')
-				})
-			}
-		);
-
-		it('should set onClose to function', () =>
-			init(flags).then(popup => {
-				popup.el.onClose.should.be.a('function')
-			})
-		);
-
-		it('should store date in local storage when closed', () =>
-			init(flags)
-				.then(popup => {
-					popup.el.onClose();
-					return localStorage.get('last-closed');
-				})
-				// give a 1s buffer
-				.then(lastClosed => lastClosed.should.be.closeTo(Date.now(), 1000))
-		);
-
-		// TODO: naughty, but errors for unknown reason - https://circleci.com/gh/Financial-Times/n-ui/2829
-		xit('should ‘pop-up’ after 2 seconds', () =>
-			init(flags)
-				.then(popup => {
-					sinon.spy(popup, 'open');
-					popup.open.should.not.have.been.called;
-					return delay(2050, popup);
-				})
-				.then(popup => popup.open.should.have.callCount(1))
-		);
-
-
-		it('should not show if last shown within 30 days', () => {
-			localStorage.set('last-closed', Date.now() + (1000 * 60 * 60 * 24 * 29));
-			return init(flags).then(popup => should.not.exist(popup));
-		});
-
-		it('should not show barrier page has not been visited in this session', () => {
-			sessionStorage.unset('last-seen');
-			return init(flags).then(popup => should.not.exist(popup));
-		});
-
-	});
-
-	// describe('"Lionel Slider" Subscription Offer Prompt - USA', () => {
+	// describe('"Lionel Slider" Subscription Offer Prompt', () => {
 
 	// 	beforeEach(() => {
 	// 		fetchStub
 	// 			.withArgs('/country')
 	// 			.returns(Promise.resolve({
-	// 				json: () => Promise.resolve('USA')
+	// 				json: () => Promise.resolve('GBR')
 	// 			}));
 	// 	});
 
-	// 	it('should have correct price when the priceFlashSale flag is on', () =>
+
+	// 	it ('should be a popup', () =>
 	// 		init(flags).then(popup => {
-	// 			popup.el.innerHTML.should.contain('$4.29')
+	// 			expect(popup).to.be.ok;
 	// 		})
 	// 	);
 
-	// 	it('should have correct price when the priceFlashSale flag is off', () => {
+
+	// 	it('should show prompt if navigated from barrier page, not on a barrier page and hasnt been shown in 30 days', () =>
+	// 		init(flags).then(popup => popup.should.be.an.instanceof(SlidingPopup))
+	// 	);
+
+	// 	it('should have correct attributes', () =>
+	// 		init(flags).then(popup => {
+	// 			popup.el.getAttribute('class').should.include('n-sliding-popup subscription-prompt');
+	// 			popup.el.getAttribute('data-n-component').should.equal('o-sliding-popup');
+	// 			popup.el.getAttribute('data-n-sliding-popup-position').should.equal('bottom left');
+	// 		})
+	// 	);
+
+	// 	it('should have correct html when the priceFlashSale flag is on', () =>
+	// 		init(flags).then(popup => {
+	// 			popup.el.innerHTML.should.contain('Save 33% now')
+	// 		})
+	// 	);
+
+	// 	it('should have correct html when the priceFlashSale flag is off', () => {
 	// 			flags = { get: (val) => val === 'b2cMessagePrompt'};
 	// 			init(flags).then(popup => {
-	// 				popup.el.innerHTML.should.contain('$4.29')
+	// 				popup.el.innerHTML.should.contain('You qualify for a 25% subscription discount')
 	// 			})
 	// 		}
 	// 	);
 
+	// 	it('should set onClose to function', () =>
+	// 		init(flags).then(popup => {
+	// 			popup.el.onClose.should.be.a('function')
+	// 		})
+	// 	);
+
+	// 	it('should store date in local storage when closed', () =>
+	// 		init(flags)
+	// 			.then(popup => {
+	// 				popup.el.onClose();
+	// 				return localStorage.get('last-closed');
+	// 			})
+	// 			// give a 1s buffer
+	// 			.then(lastClosed => lastClosed.should.be.closeTo(Date.now(), 1000))
+	// 	);
+
+	// 	// TODO: naughty, but errors for unknown reason - https://circleci.com/gh/Financial-Times/n-ui/2829
+	// 	xit('should ‘pop-up’ after 2 seconds', () =>
+	// 		init(flags)
+	// 			.then(popup => {
+	// 				sinon.spy(popup, 'open');
+	// 				popup.open.should.not.have.been.called;
+	// 				return delay(2050, popup);
+	// 			})
+	// 			.then(popup => popup.open.should.have.callCount(1))
+	// 	);
+
+
+	// 	it('should not show if last shown within 30 days', () => {
+	// 		localStorage.set('last-closed', Date.now() + (1000 * 60 * 60 * 24 * 29));
+	// 		return init(flags).then(popup => should.not.exist(popup));
+	// 	});
+
+	// 	it('should not show barrier page has not been visited in this session', () => {
+	// 		sessionStorage.unset('last-seen');
+	// 		return init(flags).then(popup => should.not.exist(popup));
+	// 	});
+
 	// });
+
+	describe('"Lionel Slider" Subscription Offer Prompt - USA', () => {
+
+		beforeEach(() => {
+			fetchStub
+				.withArgs('/country')
+				.returns(Promise.resolve({
+					json: () => Promise.resolve('USA')
+				}));
+		});
+
+		it('should have correct price when the priceFlashSale flag is on', () =>
+			init(flags).then(popup => {
+				popup.el.innerHTML.should.contain('$4.29')
+			})
+		);
+
+		it('should have correct price when the priceFlashSale flag is off', () => {
+				flags = { get: (val) => val === 'b2cMessagePrompt'};
+				init(flags).then(popup => {
+					popup.el.innerHTML.should.contain('$4.29')
+				})
+			}
+		);
+
+	});
 
 	// describe('"Lionel Slider" Subscription Offer Prompt - country code not listed', () => {
 
