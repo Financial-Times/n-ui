7175979d8206daf035932e7303b9e1e4ef57e3c4
Rhys Evans <rhys.evans@ft.com>
2017-03-07 12:50:53 +0000

    removed deprecated nav stuff

--- node/models/navigation/index.js
+++ node/models/navigation/index.js
@@ -1,17 +1,14 @@
 const log = require('@financial-times/n-logger').default;
 
-const NavigationModelV1 = require('./navigationModelV1');
 const NavigationModelV2 = require('./navigationModelV2');
 
-let navigationModelV1;
 let navigationModelV2;
 
 module.exports = {
 	init: options => {
-		navigationModelV1 = new NavigationModelV1(options);
 		navigationModelV2 = new NavigationModelV2(options);
 
-		return Promise.all([navigationModelV1.init(), navigationModelV2.init()]);
+		return navigationModelV2.init();
 	},
 	middleware : (req, res, next) => {
 		log.info({event:'NAVIGATION_MIDDLEWARE', source:'Origami'});
