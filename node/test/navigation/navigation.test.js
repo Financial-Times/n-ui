2e0758b4295ac5d042b4857cb74a7eada8ccab91
Rhys Evans <wheresrhys@gmail.com>
2017-03-07 14:26:43 +0000

    removed deprecated nav stuff (#857)

--- node/test/navigation/navigation.test.js
+++ node/test/navigation/navigation.test.js
@@ -4,24 +4,20 @@ const proxyquire = require('proxyquire').noCallThru().noPreserveCache();
 describe('Navigation middleware', () => {
 
 	let navigation;
-	let navigationModelV1Stub = {init:sinon.spy(), middleware:sinon.spy()};
 	let navigationModelV2Stub = {init:sinon.spy(), middleware:sinon.spy()};
 
 	before(() => {
-		const NavigationModelV1Stub = sinon.stub().returns(navigationModelV1Stub);
 		const NavigationModelV2Stub = sinon.stub().returns(navigationModelV2Stub);
-		navigation = proxyquire('../../models/navigation/index', {'./navigationModelV1':NavigationModelV1Stub, './navigationModelV2':NavigationModelV2Stub});
+		navigation = proxyquire('../../models/navigation/index', {'./navigationModelV2':NavigationModelV2Stub});
 	});
 
 	afterEach(() => {
-		navigationModelV1Stub.middleware.reset();
 		navigationModelV2Stub.middleware.reset();
 	});
 
-	it('Should call init() on both models', () => {
+	it('Should call init() on navigation model', () => {
 		navigation.init();
 		sinon.assert.called(navigationModelV2Stub.init);
-		sinon.assert.called(navigationModelV1Stub.init);
 	});
 
 	it('Should use the V2 Model', () => {
@@ -31,6 +27,5 @@ describe('Navigation middleware', () => {
 		navigation.init();
 		navigation.middleware(req, res, next);
 		sinon.assert.called(navigationModelV2Stub.middleware);
-		sinon.assert.notCalled(navigationModelV1Stub.middleware);
 	});
 });
