dcc65e518625f46522ab11fb53921dcb66cae08d
Laura Carvajal <laura.carvajal@gmail.com>
2017-03-14 11:32:50 +0000

    pending tests

--- tabbing/index.js
+++ tabbing/index.js
@@ -1,10 +1,18 @@
 module.exports = {
 	init: function (flags) {
-		if (flags.get('tabe')) {
-			window.addEventListener('a11yKeyboardFocus', function (e) {
-				if (e.keyCode === 9) {
-					// don't do this if element is an input
-					// find element with js class in it, add has-tabbed class to it
+
+		if (flags.get('a11yKeyboardFocus')) {
+			const container = document.querySelector('.js');
+
+			window.addEventListener('keyup', function (e) {
+				const pressedTab = e.keyCode === 9;
+				const hasHasTabbedClass = container.className.indexOf('has-tabbed') !== -1; // needed for older IE vs classList?
+
+				// Don't activate focus style when tab pressed inside a form elements
+				const activatesFocusStyle = !e.target.type;
+
+				if (pressedTab && !hasHasTabbedClass && activatesFocusStyle) {
+					container.className = container.className + ' has-tabbed';
 				}
 			});
 		}
