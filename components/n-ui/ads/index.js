e7972fd737d2fe484e21646fc209966d4fa1e5ee
Andy Newman <andygnewman@users.noreply.github.com>
2017-03-23 09:07:42 +0000

    Add: Counter ad blocking feature (#842)
    * add counter ad blcoking to ads
    
    * remove sourcepoint adblock check
    
    * include counter ad blocking css correctly
    
    * bump n-counter-ad-blocking to v0.0.4
    
    * bump n-counter-ad-blocking to 0.0.5
    
    * swap to bower n-counter-ad-blocking
    
    * set sourcepoint flag off for header a11y
    
    * log to aid debuging build failure
    
    * Revert "log to aid debuging build failure"
    
    This reverts commit 8f35c451b6cfcf3e61ced12c8df283f60aad3f94.
    
    * tidy up
    
    * switch sourcepoint off for all a11y tests
    
    * remove n-counter-ad-blocking from packagejson
    
    * bump n-counter-ad-blocking to v1
    
    * wrap adblocking in loaded event
    
    * remove sourcepoint flag from a11y config

--- components/n-ui/ads/index.js
+++ components/n-ui/ads/index.js
@@ -7,7 +7,8 @@ const Reporter = require('./js/reporter');
 const sendMetrics = require('./js/metrics');
 const Sticky = require('./js/sticky');
 
-import { perfMark } from 'n-ui-foundations'
+import nCounterAdBlocking from 'n-counter-ad-blocking';
+import { perfMark } from 'n-ui-foundations';
 
 let slotCount;
 let slotsRendered = 0;
@@ -98,6 +99,10 @@ function onAdsComplete (flags, event) {
 
 module.exports = {
 	init: (flags, appInfo, opts) => {
+
+		window.addEventListener('ftNextLoaded', function () {
+			nCounterAdBlocking.init(flags);
+		});
 
 		const adOptions = typeof opts === 'object' ? opts : {};
 
