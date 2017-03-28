b51da6dae5501dbc7ae499262ca2a1307f82dbba
Andy Gout <andygout@users.noreply.github.com>
2017-03-28 11:47:45 +0100

    Add: Anonymous user pa11y scenarios (#891)

--- components/n-ui/header/pa11y-config.js
+++ components/n-ui/header/pa11y-config.js
@@ -22,6 +22,28 @@ module.exports = {
 					'FT-Flags': 'ads:off,javascript:off'
 				}
 			}
+		},
+		{
+			rootElement: '.n-layout__row--header',
+			// Hide elements that depend on other components’ markup
+			hideElements: '.n-skip-link, [href="#o-header-drawer"]',
+			page: {
+				headers: {
+					'FT-Anonymous-User': true,
+					'FT-Flags': 'ads:off,javascript:on'
+				}
+			}
+		},
+		{
+			rootElement: '.n-layout__row--header',
+			// Hide elements that depend on other components’ markup
+			hideElements: '.n-skip-link, [href="#o-header-drawer"]',
+			page: {
+				headers: {
+					'FT-Anonymous-User': true,
+					'FT-Flags': 'ads:off,javascript:off'
+				}
+			}
 		}
 	]
 };
