830fccbee6407aacb79807c5f4473f7c9225f360
Simon Legg <leggsimon@gmail.com>
2017-06-03 16:32:13 +0100

    Builds now use main config (#966)
    * demo build to use new config
    
    * app build uses new config
    
    * no implicit return for future flexibility/settings

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,18 +1,15 @@
-'use strict';
-
-const nWebpack = require('../build/webpack');
-const headCss = require('../build/lib/head-css')
+const nWebpack = require('../build/webpack/webpack.config.js');
+const ExtractCssBlockPlugin = require('extract-css-block-webpack-plugin');
 const path = require('path');
-const webpackConfig = headCss(nWebpack({
-	withBabelPolyfills: false,
-	entry: {
-		'./public/main-without-n-ui.js': './demo/client/main.js',
-		'./public/main.css': './demo/client/main.scss'
-	},
-	includes: [
-		path.join(__dirname, '../')
-	],
-	exclude: [/node_modules/]
-}));
+const webpackConfig = nWebpack();
+
+// HACK: adds path to the babel loader config
+webpackConfig.module.loaders[0].include.push(new RegExp(path.join(__dirname, '../')))
+
+webpackConfig.plugins.push(new ExtractCssBlockPlugin());
+webpackConfig.entry = {
+	'./public/main-without-n-ui.js': './demo/client/main.js',
+	'./public/main.css': './demo/client/main.scss'
+}
 
 module.exports = webpackConfig;
