d8e99ae34a750eba36081165469c03a781ece402
Simon Legg <leggsimon@gmail.com>
2017-06-16 16:10:57 +0100

    shared app build config

--- build/app/webpack.js
+++ build/app/webpack.js
@@ -1,8 +1,9 @@
 const path = require('path');
-const nWebpack = require('../webpack/webpack.config.js');
+// const nWebpack = require('../webpack/webpack.config.js');
+// const nWebpack = require('./webpack.app.config.js');
 const fs = require('fs');
 const join = require('path').join;
-// const Wrap = require('../lib/addons/wrap');
+const Wrap = require('../lib/addons/wrap');
 const ExtractCssBlockPlugin = require('extract-css-block-webpack-plugin');
 
 const gitignore = fs.readFileSync(join(process.cwd(), '.gitignore'), 'utf8')
@@ -47,9 +48,32 @@ so this config is for all entry points defined by an app *excluding* the main.js
 
 Mostly this config will only be for main.css.
 */
-const nonMainJsWebpackConfig = nWebpack();
-nonMainJsWebpackConfig.entry = filterEntryKeys(baseConfig.entry, /main\.js$/, true);
-nonMainJsWebpackConfig.plugins.push(new ExtractCssBlockPlugin());
+
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('./webpack.app.common.js');
+
+
+// const mainJsConfig = webpackMerge(commonConfig, {
+// 	entry: {
+// 		'./public/main-without-n-ui.js': './client/main.js'
+// 	},
+// 	plugins:[
+// 		new ExtractCssBlockPlugin()
+// 	]
+// })
+
+const nonMainJsWebpackConfig = webpackMerge(commonConfig, {
+	entry: filterEntryKeys(baseConfig.entry, /main\.js$/, true),
+	plugins:[
+		new ExtractCssBlockPlugin()
+	]
+})
+
+
+// const nonMainJsWebpackConfig = nWebpack();
+// console.log('\x1b[36m', filterEntryKeys(baseConfig.entry, /main\.js$/, true);, '\x1b[0m')
+// nonMainJsWebpackConfig.entry = filterEntryKeys(baseConfig.entry, /main\.js$/, true);
+// nonMainJsWebpackConfig.plugins.push(new ExtractCssBlockPlugin());
 webpackConfigs.push(nonMainJsWebpackConfig);
 
 
@@ -63,10 +87,20 @@ has been loaded.
 */
 const nUiExternal = require('../../browser/js/webpack-entry');
 const nUiExternalPoints = nUiExternal(baseConfig.nUiExcludes);
-
-const mainJsWebpackConfig = nWebpack();
-mainJsWebpackConfig.entry = modifyEntryKeys(baseConfig.entry, /main\.js$/, name => name.replace(/\.js$/,'-without-n-ui.js'));
-mainJsWebpackConfig.externals = nUiExternalPoints;
+const mainJsWebpackConfig = webpackMerge(commonConfig, {
+	entry: modifyEntryKeys(baseConfig.entry, /main\.js$/, name => name.replace(/\.js$/,'-without-n-ui.js')),
+	externals: nUiExternalPoints,
+	plugins:[
+		new Wrap(
+			'(function(){function init(){\n',
+			'\n};window.ftNextnUiLoaded ? init() : document.addEventListener ? document.addEventListener(\'ftNextnUiLoaded\', init) : document.attachEvent(\'onftNextnUiLoaded\', init);})();',
+			{ match: /\.js$/ }
+		)
+	]
+})
+// const mainJsWebpackConfig = nWebpack();
+// mainJsWebpackConfig.entry = modifyEntryKeys(baseConfig.entry, /main\.js$/, name => name.replace(/\.js$/,'-without-n-ui.js'));
+// mainJsWebpackConfig.externals = nUiExternalPoints;
 // mainJsWebpackConfig.plugins.push(
 // 	new Wrap(
 // 		'(function(){function init(){\n',
@@ -106,10 +140,16 @@ If you do not need this behaviour run
 		throw 'Add /public/n-ui/ to your .gitignore to start building a local app shell';
 	}
 
-	const appShellWebpackConfig = nWebpack();
-	appShellWebpackConfig.entry = {
-		'./public/n-ui/es5.js': './bower_components/n-ui/build/deploy/wrapper.js'
-	};
+	const appShellWebpackConfig = webpackMerge(commonConfig, {
+		entry: {
+			'./public/n-ui/es5.js': './bower_components/n-ui/build/deploy/wrapper.js'
+		}
+	})
+
+	// const appShellWebpackConfig = nWebpack();
+	// appShellWebpackConfig.entry = {
+	// 	'./public/n-ui/es5.js': './bower_components/n-ui/build/deploy/wrapper.js'
+	// };
 	webpackConfigs.push(appShellWebpackConfig)
 }
 
