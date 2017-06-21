dd6ced83a781b44d6840c818f7e2527399332b49
Simon Legg <leggsimon@gmail.com>
2017-06-21 11:47:38 +0100

    split common config out and use webpack-merge

--- build/app/webpack.app.common.js
+++ build/app/webpack.app.common.js
@@ -1,15 +1,15 @@
 /*
-This is just porting all the configs from all the seperate transform files and
-having one large standard one. This should make it easier to update to webpack 2
-As well as just making it more accessible without having to dive around all the
-transforms. Would prefer to have a well commented file like Origami:
+This is the webpack config that is shared amongst building an individual app's js & css.
+
+It uses webpack-merge to add this config to the common config.
+If you need to add any config which will be shared amongst an app build and an
+n-ui build please add it to the common config.
 */
 
 const path = require('path');
 const glob = require('glob');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const autoprefixer = require('autoprefixer');
-
+const webpackMerge = require('webpack-merge');
+const commonConfig = require('../webpack.common.config.js');
 
 const handlebarsConfig = () => {
 	const extraHelperDirs = glob.sync('**/node_modules/@financial-times/**/handlebars-helpers')
@@ -30,123 +30,15 @@ const handlebarsConfig = () => {
 	};
 };
 
-const extractOptions = [{
-		loader: 'css-loader',
-		options: {
-			minimize: true,
-			sourceMap: true
-		}
-	},
-	{
-		loader: 'postcss-loader',
-		options: {
-			plugins: () => [
-				autoprefixer({
-					browsers: ['> 1%', 'last 2 versions', 'ie >= 9', 'ff ESR', 'bb >= 7', 'iOS >= 5'],
-					flexbox: 'no-2009'
-				})
-			]
-		}
-	},
-	{
-		loader: 'sass-loader',
-		options: {
-			sourcemap: true,
-			includePaths: [
-				path.resolve('./bower_components'),
-				path.resolve('./node_modules/@financial-times')
-			],
-			// NOTE: This line is important for preservation of comments needed by the css-extract-block plugin
-			outputStyle: 'expanded'
-		}
-	}
-]
-
-module.exports = {
-	devtool: 'source-map',
-
-	resolve: {
-		modules: [
-			'bower_components',
-			'node_modules',
-		],
-		// These JSON files are read in directories
-		descriptionFiles: ['bower.json', 'package.json'],
-
-		// These fields in the description files are looked up when trying to resolve the package directory
-		mainFields: ['main', 'browser'],
-
-		// These files are tried when trying to resolve a directory
-		mainFiles: [
-			'index',
-			'main',
-			'src/main-client' /* HACK: this is becaause of n-image's entrypoin not being main.js or index.js */
-		],
-
-		// These fields in the description files offer aliasing in this package
-		// The content of these fields is an object where requests to a key are mapped to the corresponding value
-		aliasFields: ['browser'],
-
-		alias: {
-			'react': 'preact-compat',
-			'react-dom': 'preact-compat'
-		},
-	},
-
+module.exports = webpackMerge(commonConfig, {
 	module: {
+		// These rules are added to the common ones rather than replacing them
 		rules: [
-			//babel
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				query: {
-					babelrc: false, // ignore any .babelrc in project & dependencies
-					cacheDirectory: true,
-					plugins: [
-						// converts `export default 'foo'` to `exports.default = 'foo'`
-						require.resolve('babel-plugin-add-module-exports'),
-
-						// ensures a module reqired multiple times is only transpiled once and
-						// is shared by all that use it rather than transpiling it each time
-						[ require.resolve('babel-plugin-transform-runtime'),
-							{
-								helpers: false,
-								polyfill: false,
-							}
-						],
-
-						// This is actually included in the 'es2015' preset but we need to override the
-						// `loose` option to be true
-						// TODO: stop transform-es2015-classes being loose. loose allows non-spec compliant classes.
-						[ require.resolve('babel-plugin-transform-es2015-classes'), { loose: true } ]
-
-						// converts import/export to commonjs, currently not used but
-						// will look to include it for browsers that can support modules
-						// require('babel-plugin-transform-es2015-modules-commonjs'),
-					],
-					presets: ['es2015', 'react']
-				}
-			},
 			{
 				test: /\.html$/,
 				loader: 'handlebars-loader',
 				options: handlebarsConfig()
-			},
-			{
-				test: /\.scss$/,
-				loader: ExtractTextPlugin.extract({
-					use: extractOptions
-				})
 			}
 		]
-	},
-
-	plugins: [
-		new ExtractTextPlugin('[name]'),
-	],
-
-	output: {
-		filename: '[name]',
-		devtoolModuleFilenameTemplate: 'n-ui//[resource-path]?[loaders]'
 	}
-};
+});
--- build/deploy/webpack.deploy.config.js
+++ build/deploy/webpack.deploy.config.js
@@ -1,162 +1,33 @@
 /*
-This is the webpack config for the pre built n-ui assets.
+This is the webpack config that is bespoke to the pre built n-ui assets.
+
+It uses webpack-merge to add this config to the common config.
+If you need to add any config which will be shared amongst an n-ui build and an
+app build please add it to the common config.
 */
 
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpackMerge = require('webpack-merge');
 const ExtractCssBlockPlugin = require('extract-css-block-webpack-plugin');
-const autoprefixer = require('autoprefixer');
+const commonConfig = require('../webpack.common.config.js');
 
-const extractOptions = [
-	{
-		loader: 'css-loader',
-		options: {
-			minimize: true,
+module.exports = webpackMerge(commonConfig, {
+
+	entry: {
+		'./dist/assets/es5.js': './build/deploy/wrapper.js',
+		'./dist/assets/es5.min.js': './build/deploy/wrapper.js',
+		'./dist/assets/n-ui-core.css': './build/deploy/shared-head.scss',
+	},
+
+	// These plugins are added to the common plugins rather than replacing them
+	plugins: [
+		// splits one stylesheet into multiple stylesheets based on nUiStylesheetStart/End comments
+		new ExtractCssBlockPlugin(),
+
+		// Minify & Uglify all the JS
+		new webpack.optimize.UglifyJsPlugin({
+			test: /\.min\.js$/,
 			sourceMap: true
-		}
-	},
-	{
-		loader: 'postcss-loader',
-		options: {
-			plugins: () => [
-				autoprefixer({
-					browsers: ['> 1%', 'last 2 versions', 'ie >= 9', 'ff ESR', 'bb >= 7', 'iOS >= 5'],
-					flexbox: 'no-2009'
-				})
-			]
-		}
-	},
-	{
-		loader: 'sass-loader',
-		options: {
-			sourcemap: true,
-
-			// An array of paths that LibSass can look in to attempt to resolve your @import declarations
-			includePaths: [ './bower_components' ],
-
-			// Determines the output format of the final CSS style.
-			// NOTE: This line is important for preservation of comments needed by the css-extract-block plugin
-			outputStyle: 'expanded'
-		}
-	}
-]
-
-module.exports = function () {
-	return {
-		// Abort the compilation on first error
-		bail: true,
-
-		// Generate source maps
-		devtool: 'source-map',
-
-		entry: {
-			'./dist/assets/es5.js': './build/deploy/wrapper.js',
-			'./dist/assets/es5.min.js': './build/deploy/wrapper.js',
-			'./dist/assets/n-ui-core.css': './build/deploy/shared-head.scss',
-		},
-
-		resolve: {
-			// In which folders the resolver look for modules relative paths are
-			// looked up in every parent folder (like node_modules) absolute
-			// paths are looked up directly the order is respected
-			modules: [
-				'bower_components',
-				'node_modules',
-			],
-
-			// These JSON files are read in directories
-			descriptionFiles: ['bower.json', 'package.json'],
-
-			// These fields in the description files are looked up when trying to resolve the package directory
-			mainFields: ['main'],
-
-			// These files are tried when trying to resolve a directory
-			mainFiles: [
-				'index',
-				'main',
-				'src/main-client' /* HACK: this is becaause of n-image's entrypoin not being main.js or index.js */
-			],
-
-			// These fields in the description files offer aliasing in this package
-			// The content of these fields is an object where requests to a key are mapped to the corresponding value
-			aliasFields: ['browser'],
-
-			// These extensions are tried when resolving a file
-			extensions: ['.js', '.json'],
-
-			// any references made to react or react-dom, use preact-compat instead
-			alias: {
-				'react': 'preact-compat',
-				'react-dom': 'preact-compat'
-			},
-		},
-
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							// ignore any .babelrc in project & dependencies
-							babelrc: false,
-							cacheDirectory: true,
-							plugins: [
-								// converts `export default 'foo'` to `exports.default = 'foo'`
-								require.resolve('babel-plugin-add-module-exports'),
-
-								// ensures a module reqired multiple times is only transpiled once and
-								// is shared by all that use it rather than transpiling it each time
-								[ require.resolve('babel-plugin-transform-runtime'),
-									{
-										helpers: false,
-										polyfill: false,
-									}
-								],
-
-								// This is actually included in the 'es2015' preset but we need to override the
-								// `loose` option to be true
-								// TODO: stop transform-es2015-classes being loose. loose allows non-spec compliant classes.
-								[ require.resolve('babel-plugin-transform-es2015-classes'), { loose: true } ]
-
-								// converts import/export to commonjs, currently not used but
-								// will look to include it for browsers that can support modules
-								// require('babel-plugin-transform-es2015-modules-commonjs'),
-							],
-							presets: ['es2015', 'react']
-						}
-					}
-				},
-				{
-					test: /\.scss$/,
-					loader: ExtractTextPlugin.extract({
-						use: extractOptions
-					})
-				}
-			]
-		},
-
-		plugins: [
-			// moves css out of JS into stylesheets
-			new ExtractTextPlugin('[name]'),
-
-			// splits one stylesheet into multiple stylesheets based on nUiStylesheetStart/End comments
-			new ExtractCssBlockPlugin(),
-
-			// replace any instances of `process.env.NODE_ENV` with `'production'`
-			// new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': '"production"' } }), // Only production
-
-			// Minify & Uglify all the JS
-			new webpack.optimize.UglifyJsPlugin({
-				test: /\.min\.js$/,
-				sourceMap: true
-			}),
-		],
-
-		output: {
-			filename: '[name]',
-			devtoolModuleFilenameTemplate: 'n-ui//[resource-path]?[loaders]'
-		}
-	};
-
-}
+		}),
+	],
+});
