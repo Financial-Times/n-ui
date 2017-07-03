028a425d350ee53e2b32c03f414ca34d6a29d303
Rhys Evans <wheresrhys@gmail.com>
2017-07-03 22:38:01 +0100

    tweak script order

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -3,12 +3,11 @@ const commonConfig = require('../build/webpack.common.config.js');
 const ExtractCssBlockPlugin = require('extract-css-block-webpack-plugin');
 const webpackEntryPoints = require('../build/webpack-entry-points');
 const nUiExternal = require('../build/webpack-externals');
-const nUiExternalPoints = nUiExternal();
 
 module.exports = [
 	webpackMerge(commonConfig, {
 		entry: webpackEntryPoints.demo,
-		externals: nUiExternalPoints,
+		externals: nUiExternal(),
 		plugins: [
 			// splits one stylesheet into multiple stylesheets based on nUiStylesheetStart/End comments
 			new ExtractCssBlockPlugin(),
