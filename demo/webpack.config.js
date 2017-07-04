14d1f951e0c901165dafdc11f22566d6c1c751ff
Rhys Evans <rhys.evans@ft.com>
2017-07-04 16:59:16 +0100

    fixed path

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,7 +1,7 @@
 const webpackMerge = require('webpack-merge');
 const commonConfig = require('../build/webpack.common.config.js');
 const ExtractCssBlockPlugin = require('extract-css-block-webpack-plugin');
-const appShellEntryPoints = require('../app-shell-entry-points');
+const appShellEntryPoints = require('../build/app-shell-entry-points');
 const nUiExternal = require('../build/webpack-externals');
 
 module.exports = [
