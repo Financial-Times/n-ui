e8c220f077748c15715153071ce71cb04537b09b
Rhys Evans <wheresrhys@gmail.com>
2017-07-04 15:20:15 +0100

    Revert "Revert "use deferred scripts instead of custom async script handling"" (#1006)
    * Revert "stop unnescessary slow down (#1001)"
    
    This reverts commit a5005433b778c53706a8c3778287cf3a47fb6778.
    
    * Revert "Revert "use deferred scripts instead of custom async script handling" (#1005)"
    
    This reverts commit fdeab11114e2caa675dd503aac21447c47f0cef6.
    
    * fix asynchronous loading of scripts

--- browser/js/loader.js
+++ browser/js/loader.js
@@ -92,37 +92,35 @@ class JsSetup {
 
 	bootstrap (opts, callback) {
 		opts = opts || {};
-		waitForCondition('Polyfill', () => {
-			this.initResult = this.initResult || this.init();
+		this.initResult = this.initResult || this.init();
 
-			this.bootstrapResult = this.initResult
-				.then(result => {
-					let promise = callback(result);
-					if (!(promise && typeof promise.then === 'function')) {
-						promise = Promise.resolve();
-					}
-					return promise
-						.then(() => {
-							if (opts.preload) {
-								perfMark('nUiJsExecuted');
-							} else {
-								document.documentElement.classList.add('js-success');
-								perfMark('appJsExecuted');
-								dispatchLoadedEvent();
-							}
-						});
-				})
-				.catch(err => {
-					if (!this.appInfo.isProduction){
-						if (typeof err === 'object' && err.stack) {
-							console.error(err.stack); //eslint-disable-line
+		this.bootstrapResult = this.initResult
+			.then(result => {
+				let promise = callback(result);
+				if (!(promise && typeof promise.then === 'function')) {
+					promise = Promise.resolve();
+				}
+				return promise
+					.then(() => {
+						if (opts.preload) {
+							perfMark('nUiJsExecuted');
 						} else {
-							console.error(err); //eslint-disable-line
+							document.documentElement.classList.add('js-success');
+							perfMark('appJsExecuted');
+							dispatchLoadedEvent();
 						}
+					});
+			})
+			.catch(err => {
+				if (!this.appInfo.isProduction){
+					if (typeof err === 'object' && err.stack) {
+						console.error(err.stack); //eslint-disable-line
+					} else {
+						console.error(err); //eslint-disable-line
 					}
-					oErrors.error(err);
-				});
-		});
+				}
+				oErrors.error(err);
+			});
 	}
 
 	loadScript (src) {
