6ab2b52c3094cedac1b2d61322033a258b60f7b1
Simon Legg <leggsimon@gmail.com>
2017-08-02 12:41:17 +0100

    add postcss-discard-duplicates  🐿 v2.5.13

--- scripts/build-sass.sh
+++ scripts/build-sass.sh
@@ -2,4 +2,4 @@
 
 rm -rf public/*.css
 node-sass ${1:-client/main.scss} --output-style compressed --include-path bower_components --include-path node_modules/@financial-times | \
-postcss --no-map --use autoprefixer postcss-extract-css-block --autoprefixer.browers "> 1% last 2 versions ie >= 9 ff ESR bb >= 7 iOS >= 5" --output ${2:-public/main.css}
+postcss --no-map --use autoprefixer postcss-discard-duplicates postcss-extract-css-block --autoprefixer.browers "> 1% last 2 versions ie >= 9 ff ESR bb >= 7 iOS >= 5" --output ${2:-public/main.css}
