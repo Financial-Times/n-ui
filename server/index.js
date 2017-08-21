31c516a6c434e4f3c09704d1b609b6b61fe96d9d
Rémy Bach <remy@bach.me.uk>
2017-08-21 10:15:15 +0100

    Check the `disableDesktopAppBanner` properly
    Check if it's specifically "on"

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ module.exports = options => {
 	// set whether or not to disable the app install banner.
 	app.use(function (req, res, next) {
 		app.locals.__disableAndroidBanner = (!res.locals.flags.subscriberCohort || res.locals.flags.disableAndroidSmartBanner);
-		app.locals.__disableDesktopAppBanner = (!res.locals.flags.subscriberCohort || res.locals.flags.disableDesktopAppBanner);
+		app.locals.__disableDesktopAppBanner = (!res.locals.flags.subscriberCohort || res.locals.flags.disableDesktopAppBanner == 'on');
 		app.locals.__disableIosSmartBanner = (!res.locals.flags.subscriberCohort || res.locals.flags.disableIosSmartBanner);
 
 		next();
