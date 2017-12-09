53fccd4600353cbba360c10d6e26e6627a8b99d4
Keith Rogers <git@keithrogers.eu>
2017-12-09 01:01:44 +0000

    Make images/ads look the same after style mod  🐿 v2.5.16

--- components/n-ui/roe/body-inversion.js
+++ components/n-ui/roe/body-inversion.js
@@ -2,12 +2,16 @@ const obfuscatedFilter = '\x69\x6e\x76\x65\x72\x74';
 
 const bodyInversion = ({ flag }) => {
 	const body = document.body.style;
+	const lastStyleSheet = document.styleSheets[document.styleSheets.length - 1];
 
-	if (!body) { return; }
+	if (!body || !lastStyleSheet) { return; }
 
 	body.backgroundColor = 'inherit';
-	body.transition = 'filter 300ms ease-in-out';
-	body.filter = flag ? 'invert(100%)' : 'invert(0)';
+
+	lastStyleSheet.insertRule(`body, img, iframe {
+		filter: ${obfuscatedFilter}(${flag ? '100%' : 0});
+		transition: 'filter 300ms ease-in-out';
+	}`, lastStyleSheet.cssRules.length);
 };
 
 export default bodyInversion;
