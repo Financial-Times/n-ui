f19ec9aa6b4d5143047251b9a2f263180a0c1de1
Ben Fletcher <ben.fletcher@ft.com>
2018-01-02 14:46:32 +0000

    add unit test for this dynamic stuff  🐿 v2.6.0

---
+++ server/test/current-year.test.js
@@ -0,0 +1,39 @@
+const request = require('supertest');
+const nextExpress = require('../index');
+const expect = require('chai').expect;
+
+describe('current year middleware', () => {
+	let app;
+	let locals;
+
+	before(() => {
+		app = nextExpress({
+			systemCode: 'server unit tests' // mandatory option
+		});
+		app.get('/', function (req, res) {
+			locals = res.locals;
+			res.sendStatus(200).end();
+		});
+	});
+
+	it('should set the res.locals.currentYear property', (done) => {
+		request(app)
+			.get('/')
+			.expect(() => {
+				expect(locals).to.have.own.property('currentYear');
+			})
+			.end(done);
+	});
+
+	it('should set the res.locals.currentYear property to 2018', (done) => {
+		request(app)
+			.get('/')
+			.expect(() => {
+				expect(locals).to.have.own.property('currentYear');
+				// this will break every year, but better to be sure it's working
+				// than using the same code logic to validate the year
+				expect(locals.currentYear).to.equal(2018);
+			})
+			.end(done);
+	});
+});
