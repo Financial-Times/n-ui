0764d95fba2856d06d64040953488e1122cdf575
Ben Fletcher <bjfletcher@gmail.com>
2018-01-11 16:46:34 +0000

    a safer way to stringify error object (#1148)
    * a safer way to stringify error object
     🐿 v2.6.0
    
    * use String() instead
     🐿 v2.6.0
    
    * a clearer version
     🐿 v2.6.0
    
    * clearer regex (if I understand the intention correct...)
     🐿 v2.6.0

--- browser/bundles/o-errors.js
+++ browser/bundles/o-errors.js
@@ -15,10 +15,19 @@ oErrors.init({
 		appName: appInfo.name
 	},
 	filterError: function (reportedObject) {
-		// Supress errors if they contain an "Undefined not a.... evaluating window.FT.flags | .nUi | .ftNextUi type error"
-		const e = reportedObject.error.toString();
-		const windowFtError = !!e.match(/^(?=.*\bundefined\b)(?=.*(\bwindow.FT.flags\b|\bwindow.FT.nUi\b|\bwindow.FT.ftNextUi\b)).*$/gi);
-		return !(window.FT.disableOErrors || windowFtError);
+		// does the error contain an "undefined" followed by one of the below?
+		// "window.FT.flags", "window.FT.nUi" or "window.FT.ftNextUi"
+		let windowFtError;
+		if ('error' in reportedObject) {
+				try {
+					windowFtError = String(reportedObject.error).match(/^.*\bundefined\b.*(\bwindow.FT.flags\b|\bwindow.FT.nUi\b|\bwindow.FT.ftNextUi\b).*$/i);
+				} catch (err) {
+					// could not stringify the error
+				}
+		}
+		// ignore if yes, or if o-errors is disabled
+		const ignore = windowFtError || window.FT.disableOErrors;
+		return !ignore;
 	},
 	errorBuffer: window.errorBuffer || []
 });
