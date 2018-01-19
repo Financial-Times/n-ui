307b65419c2323ade56a558671fb40c77d0dc13d
Braimbridge, Adam <Adam.Braimbridge@ft.com>
2018-01-19 13:15:39 +0000

    Fix: Allow node-sass and postcss to handle different filenames.  🐿 v2.7.0

--- scripts/build-sass.sh
+++ scripts/build-sass.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+# Note: ${1##*/} <-- returns the file name from a path
+# E.g. ./client/comments.scss returns comments.scss
+
 node-sass ${1:-client/main.scss} \
 --output-style compressed \
 --include-path bower_components \
 --include-path node_modules/@financial-times \
 --output ./tmp \
 && \
-postcss ./tmp/main.css \
+scssfile=${1##*/}
+cssfile=${scssfile//"scss"/"css"}
+postcss ./tmp/${cssfile:-main.css} \
 --no-map \
 --use postcss-discard-duplicates autoprefixer postcss-extract-css-block \
 --autoprefixer.browsers "> 1% last 2 versions ie >= 9 ff ESR bb >= 7 iOS >= 5" \
 --output ${2:-public/main.css} \
 && \
-rm -rf tmp \
+rm -rf ./tmp/${cssfile:-main.css}
