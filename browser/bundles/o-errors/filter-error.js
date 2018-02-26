8441ab20afc5ce99fbba2ee45268635665b488fc
Dora Militaru <dora.militaru@ft.com>
2018-02-26 19:41:29 +0000

    Dora/fix sentry logging (#1193)
    * some optimisation to error filtering
     🐿 v2.7.0
    
    * lint
     🐿 v2.7.0

--- browser/bundles/o-errors/filter-error.js
+++ browser/bundles/o-errors/filter-error.js
@@ -1,16 +1,21 @@
-module.exports = (reportedObject) => {
+module.exports = ({ error }) => {
 	// we want to filter out errors that only occur
 	// when critical scripts fail to load - in that case
 	// the execution of JS is halted and we fall back to core
-	const errorFilter = /\bwindow\.FT\.(flags|nUi|ftNextUi) is undefined|\bundefined\b.*window\.FT\.(flags|nUi)/i;
+	const errorFilters = [
+		/window\.FT\.(flags|nUi|ftNextUi)/i,
+		/undefined is|is undefined/i
+	];
 	let windowFtError;
-	if ('error' in reportedObject) {
-			try {
-				windowFtError = !!String(reportedObject.error).match(errorFilter);
-			} catch (err) {
-				// could not stringify the error
-			}
+	if(error) {
+		try {
+			let errorString = String(error);
+			windowFtError = errorFilters.every(rx => rx.test(errorString));
+		} catch (err) {
+			// could not stringify the error
+		}
 	}
+
 	// filter if yes, or if o-errors disabled
 	return !(windowFtError || window.FT.disableOErrors);
 };
