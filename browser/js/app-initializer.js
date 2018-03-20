6c5d8acb35efef83bee8fe96272f334199619b1d
Remy Bach <remy@bach.me.uk>
2018-03-20 13:18:30 +0000

    Suppress service worker unavailable errors

--- browser/js/app-initializer.js
+++ browser/js/app-initializer.js
@@ -104,7 +104,13 @@ export class AppInitializer {
 				.register(flags)
 				.catch(() => { });
 
-			serviceWorker.message({ type: 'updateCache', data: {}});
+			serviceWorker
+				.message({ type: 'updateCache', data: {}})
+				.catch((err) => {
+					if (err.toLowerCase() !== 'service worker unavailable') {
+						return Promise.reject(err);
+					}
+				});
 		} else {
 			serviceWorker.unregister();
 		}
