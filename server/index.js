a53887bdd42f3d8d08f339dc03eaa6b01cbde84e
Courtney Osborn <courtney.osborn@ft.com>
2018-05-09 14:17:13 +0100

    Remove disable flags for iso and android (#1242)
    * Removes disableAndroidSmartBanner and disableIosSmartBanner flag from code
     🐿 v2.8.0
    
    * Combines __disableAndroidBanner and __disableIosSmartBanner into one variable __disableMobilePhoneBanner
     🐿 v2.8.0

--- server/index.js
+++ server/index.js
@@ -73,8 +73,7 @@ module.exports = options => {
 
 	// set whether or not to disable the app install banner.
 	app.use(function (req, res, next) {
-		app.locals.__disableAndroidBanner = (!res.locals.flags.subscriberCohort || res.locals.flags.disableAndroidSmartBanner);
-		app.locals.__disableIosSmartBanner = (!res.locals.flags.subscriberCohort || res.locals.flags.disableIosSmartBanner);
+		app.locals.__disableMobilePhoneBanner = !res.locals.flags.subscriberCohort;
 		next();
 	});
 
