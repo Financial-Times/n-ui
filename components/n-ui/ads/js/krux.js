301befe67e7b5cc075e7bffe291253e055107617
Andrew Georgiou <andrewgeorgiou1981@gmail.com>
2018-05-17 11:24:40 +0100

    Removing Krux feature flags as features are no longer required  🐿 v2.8.0

--- components/n-ui/ads/js/krux.js
+++ components/n-ui/ads/js/krux.js
@@ -38,36 +38,17 @@ exports.init = (flags) => {
 			frequencyCap('doubleclick', 2, () => { addPixel('https://usermatch.krxd.net/um/v2?partner=google'); });
 			}
 
-
 			// The following four scripts are user matching scripts for Kruxdata partners
 			// They provide 3rd party data on user Demographic (eg:	Males),	Intent	(eg:	Children's	Apparel	Buyers)
 			if(flags.get('kruxAcxiomIntegration')) {
 				frequencyCap('acxiom', 2, () => { addPixel(`https://idsync.rlcdn.com/379708.gif?partner=${kuid}`); });
 			}
 
-			if(flags.get('kruxDataLogixIntegration')) {
-				frequencyCap('DataLogix', 1, () => {
-					const kurlParams = encodeURIComponent(`_kuid=${kuid}&_kdpid=2dd640a6-6ebd-4d4f-af30-af8baa441a0d&dlxid=<na_id>&dlxdata=<na_da>`);
-					const kurl = `https://beacon.krxd.net/data.gif?${kurlParams}`;
-					addPixel(`https://r.nexac.com/e/getdata.xgi?dt=br&pkey=gpwn29rvapq62&ru=${kurl}`);
-				});
-			}
-
-			if(flags.get('kruxIXIIntegration')) {
-				frequencyCap('ixidigital', 2, () => { addPixel('https://kr.ixiaa.com/C726AB29-0470-440B-B8D2-D552CED3A3DC/a.gif'); });
-			}
-
-
 			if(flags.get('kruxExelateIntegration')) {
 				frequencyCap('exelate', 2, () => {
 					addPixel(`https://loadm.exelator.com/load?_kdpid=e4942ff0-4070-4896-a7ef-e6a5a30ce9f9&buid=${kuid}&p=204&g=270&j=0`);
 				});
 			}
-		}
-
-		//AdAdvisor is another Krux data partner with 3rd party data points
-		if(flags.get('kruxAdAdvisorIntegration')) {
-			frequencyCap('AdAdvisor-S2S', 2, () => { addPixel('https://adadvisor.net/adscores/g.js?sid=9212244187&_kdpid=2111c0af-fc3a-446f-ab07-63aa74fbde8e'); });
 		}
 
 	});
