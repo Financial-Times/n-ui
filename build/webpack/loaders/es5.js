82fbcb1c2641af0d1a7f16b4a1c4930e04dd8552
doramatadora <dora.militaru@ft.com>
2018-06-27 00:24:18 +0100

    replace regenerator with fast-async and nodent  🐿 v2.9.0

--- build/webpack/loaders/es5.js
+++ build/webpack/loaders/es5.js
@@ -12,23 +12,27 @@ const babelLoaderConfig = () => ({
 		babelrc: false,
 		cacheDirectory: true,
 		plugins: [
-			// converts `export default 'foo'` to `exports.default = 'foo'`
-			require.resolve('babel-plugin-add-module-exports'),
-			// includes Babel's regenerator	runtime (once only)
-			// for client-side async/await support
 			[
-				require.resolve('babel-plugin-transform-runtime'),
+				// use fast-async and nodent instead of Babel's regenerator
+				// https://github.com/MatAtBread/fast-async
+				// it's 3-4x faster in a browser (up to 10x on mobile)
+				'fast-async',
 				{
-					helpers: false,
-					polyfill: false
+					// place nodent runtime in app shell bundles 
+					// where 'use runtime-nodent'; is specified
+					// if async/await is present
+					'runtimePattern': 'directive'
 				}
-			]
+			],
+			// converts `export default 'foo'` to `exports.default = 'foo'`
+			require.resolve('babel-plugin-add-module-exports')
 		],
 		presets: [
 			[
 				require.resolve('babel-preset-env'),
 				{
 					include: ['transform-es2015-classes'],
+					exclude: ['transform-regenerator', 'transform-async-to-generator'],
 					targets: {
 						browsers: ['last 2 versions', 'ie >= 11']
 					}
