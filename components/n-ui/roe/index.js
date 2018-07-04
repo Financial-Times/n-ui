5447de7f35fdcfa479cfde3ccf3feb579d585383
Simon Legg <leggsimon@gmail.com>
2018-07-04 11:03:48 +0100

    🏳️‍🌈  🐿 v2.9.0

--- components/n-ui/roe/index.js
+++ components/n-ui/roe/index.js
@@ -1,11 +1,16 @@
 import WordListener from 'word-listener';
 import { createToggler } from 'n-ui-foundations';
 import bodyInversion from './body-inversion';
+import rainbowize from './rainbow';
 
 function init () {
 	new WordListener({
 		word: '\x69\x6e\x76\x65\x72\x74',
 		callback: createToggler({ callback: bodyInversion })
+	});
+	new WordListener({
+		word: '\x70\x72\x69\x64\x65',
+		callback: createToggler({ callback: rainbowize })
 	});
 }
 
