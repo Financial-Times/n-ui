3e8c5d275a0fc3ce00dbf7e7580918158c9541fc
sal dixon <sally.dixon@ft.com>
2019-01-17 11:40:03 +0000

    Convert spaces to tabs  🐿 v2.12.0

--- components/n-ui/tracking/ft/utils/abTestHelpers.js
+++ components/n-ui/tracking/ft/utils/abTestHelpers.js
@@ -4,11 +4,11 @@ const getTeaserTestContext = function (doc) {
 	const teasersUnderTest = [].slice.call(doc.querySelectorAll('[data-trackable-context-teaser-variant]'));
 
 	const transformedTeasers = teasersUnderTest.map(teaser => 
-     ({
-				content_id: teaser.getAttribute('data-content-id'),
-				variant: teaser.getAttribute('data-trackable-context-teaser-variant'),
-				headline_text: teaser.innerText
-    })
+		({
+			content_id: teaser.getAttribute('data-content-id'),
+			variant: teaser.getAttribute('data-trackable-context-teaser-variant'),
+			headline_text: teaser.innerText
+		})
 	);
 
 	return transformedTeasers;
