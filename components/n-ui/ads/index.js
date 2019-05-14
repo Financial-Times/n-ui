e0e5838b96a5f52d261b5517ad218b8d799cee58
Andrew Georgiou <andrewgeorgiou1981@gmail.com>
2019-05-14 12:03:32 +0100

    WIP - adding the Beta origami o-permutive component to enable a trial of Permutives functionality

--- components/n-ui/ads/index.js
+++ components/n-ui/ads/index.js
@@ -67,7 +67,21 @@ export default {
 			nCounterAdBlocking.init(flags);
 		});
 		if (flags && flags.get('AdsPermutive')) {
-			oPermutive.init();
+			let oPermConf = {
+				"appInfo" : {
+						"appName" : "article",
+						"contentId" : "5cfae92e-6cc5-11e9-80c7-60ee53e6681d"
+					},
+					"publicApiKeys" : {
+						"id" : "e1c3fd73-dd41-4abd-b80b-4278d52bf7aa",
+						"key" : "b2b3b748-e1f6-4bd5-b2f2-26debc8075a3"
+					},
+					"adsApi" : {
+						"user" : "https://ads-api.ft.com/v1/user",
+						"content" : "https://ads-api.ft.com/v1/content/"
+					}
+				}
+			oPermutive.init(false, oPermConf);
 		}
 		const adOptions = typeof opts === 'object' ? opts : {};
 
