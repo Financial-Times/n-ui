2320c54eb29ec3f58cfcbb7b4db85612464edb14
Alex Florisca <alexflorisca@gmail.com>
2019-06-03 13:43:24 +0100

    Upgrade o-permutive (#1439)
    * permutive implementation
     🐿 v2.12.3
    
    * Update to latest o-permutive
     🐿 v2.12.3
    
    * o-permutive working in n-ui
     🐿 v2.12.3
    
    * Update o-permutive to fix tests
     🐿 v2.12.3

---
+++ components/n-ui/ads/js/oPermutiveConfig.js
@@ -0,0 +1,67 @@
+/**
+ * Return a configuration object for o-permutive module
+ *
+ * @param {String} appName Name of the app loading n-ui
+ * @param {String} contentId UUID of article page
+ */
+export function getOPermutiveConfig () {
+	return {
+		projectId: 'e1c3fd73-dd41-4abd-b80b-4278d52bf7aa',
+		publicApiKey : 'b2b3b748-e1f6-4bd5-b2f2-26debc8075a3', // public, it's ok, don't panic.
+		consentFtCookie: true
+	};
+}
+
+/**
+ * Get meta data about the user and current page and format it for o-permutive
+ *
+ * Note: This is currently done by accessing the properties set by o-ads and krux.
+ * This should change so that the fetch to the ads-api is done first, and the data passed
+ * to o-ads and o-permutive respectively.
+ *
+ * @param {String} appName Name of the app loading n-ui
+ */
+export function getOPermutiveMetaData (appName, kruxMeta, contentId = null) {
+	let pageMeta = {};
+	let userMeta = {};
+
+	const kruxUserMeta = kruxMeta.user;
+
+	if(appName === 'article') {
+		const kruxPageMeta = kruxMeta.page;
+
+		if(contentId) {
+			pageMeta.id = contentId;
+		}
+
+		if(kruxPageMeta) {
+			const type = Array.isArray(kruxPageMeta.genre) && kruxPageMeta.genre.length > 0 ? kruxPageMeta.genre[0] : null;
+
+			pageMeta = {
+					id: contentId,
+					type: type,
+					organisations: kruxPageMeta.organisations,
+					people: kruxPageMeta.people,
+					categories: kruxPageMeta.ca,
+					authors: kruxPageMeta.authors,
+					topics: kruxPageMeta.topics,
+					admants: kruxPageMeta.ad
+			};
+		}
+	}
+
+	if(kruxUserMeta) {
+		userMeta = {
+			industry: kruxUserMeta.industry,
+			position: kruxUserMeta.job_position,
+			responsibility: kruxUserMeta.job_responsibility
+		};
+	}
+
+
+	return {
+		type: appName,
+		article: pageMeta,
+		user: userMeta
+	};
+}
